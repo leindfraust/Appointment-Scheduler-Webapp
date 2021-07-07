3ccf2ecebe800945d667d20bf96ca1a6dd718534
Leindfraust <rozer223@gmail.com>
2021-07-07 17:45:02 +0800

    remove sort and beautified

--- routes/api/adminList.js
+++ routes/api/adminList.js
@@ -6,10 +6,7 @@ router.get('/', async (req, res) => {
     try {
         const adminList = await Admin.find()
         if (!adminList) throw new Error('no items')
-        const sorted = adminList.sort((a, b) => {
-            return new Date(a.date).getTime() - new Date(b.date).getTime()
-        })   
-        res.status(200).json(sorted)
+        res.status(200).json(adminList)
     } catch (error) {
         res.status(500).json({
             message: error.message
@@ -18,38 +15,49 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const newAdmin = new  Admin(req.body)
+    const newAdmin = new Admin(req.body)
     try {
         const adminList = await newAdmin.save()
         if (!adminList) throw new Error('Cannot save')
         res.status(200).json(adminList)
-    } catch(err){
-         res.status(500).json({ message: err.message})
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
     }
 });
 
-router.put('/:id', async (req,res) => {
+router.put('/:id', async (req, res) => {
     const {
         id
     } = req.params
     try {
         const response = await Admin.findByIdAndUpdate(id, req.body)
-        if(!response) throw new Error('cannot update')
-        const updated = {...response._doc, ...req.body}
+        if (!response) throw new Error('cannot update')
+        const updated = {
+            ...response._doc,
+            ...req.body
+        }
         res.status(200).json(updated)
-    } catch(err) {
-        res.status(500).json({message: err.message})
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
     }
 });
 
-router.delete('/:id', async(req, res) => {
-    const { id } = req.params
+router.delete('/:id', async (req, res) => {
+    const {
+        id
+    } = req.params
     try {
         const removed = await Admin.findByIdAndDelete(id)
-        if(!removed) throw new Error('something went wrong, try again later')
+        if (!removed) throw new Error('something went wrong, try again later')
         res.status(200).json(removed)
-    } catch(err) {
-        res.status(500).json({message: err.message})
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
     }
 });
 
