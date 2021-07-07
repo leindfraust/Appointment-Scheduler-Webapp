e74968fbbfbe10dd873b645cd3a73938bbc55eeb
Leindfraust <rozer223@gmail.com>
2021-07-07 17:46:03 +0800

    added id for session user and schedule

--- sessions/users/user.js
+++ sessions/users/user.js
@@ -6,12 +6,18 @@ router.get('/',(req, res) => {
 });
 
 router.post('/', (req, res) => {
+    req.session._id = req.body._id
     req.session.alias = req.body.alias
     req.session.username = req.body.username
     req.session.password = req.body.password
+    req.session.schedule = req.body.schedule
     res.end()
 });
 
+router.put('/', (req, res) => {
+    req.session.schedule = req.body.schedule
+    res.end()
+})
 router.delete('/', (req, res) => {
     req.session.destroy()
     res.end()
