6b940fc83aca263019b2c8b3d3d7d31f5736a0bc
Leindfraust <rozer223@gmail.com>
2021-07-17 23:02:22 +0800

    Mobile friendliness, proper passing values to vuex

--- appointment-scheduler-webapp/src/components/DoctorList.vue
+++ appointment-scheduler-webapp/src/components/DoctorList.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="card container" style="width: 50%">
+  <div class="card container doctorContainer">
     <div class="block">
       <div class="card-image">
-        <div class="columns">
+        <div class="columns is-mobile">
           <div class="column" v-for="specialist in specialistList" :key="specialist._id">
-            <figure class="image is-4by3">
+            <a @click="getDoctors(specialist.specialization)"><figure class="image is-4by3">
               <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
+                :src="require(`../assets/specialization/${specialist.specialization}.jpg`)"
               />
-            </figure>
-            <h1 class="subtitle has-text-centered">{{ specialist.specialist }}</h1>
+            </figure></a>
           </div>
         </div>
-      </div>
+      </div><hr>
       <div class="card-content" v-for="doctors in doctorList" :key="doctors._id">
         <div class="media">
           <figure class="media-left">
@@ -26,6 +24,7 @@
           <div class="media-content">
             <p class="title is-4">{{ doctors.name }}</p>
             <p class="subtitle is-6">{{ doctors.specialist }}</p>
+            <button class="button" @click="pickDoctor(doctors._id, doctors.schedule)">Appoint</button>
           </div>
         </div>
       </div>
@@ -35,43 +34,51 @@
 
 <script>
 import axios from "axios";
+import store from '../store'
 
 export default {
   name: "DoctorList",
 
   data() {
     return {
-      image: "../assets/doctors/Timay.jpg",
       doctorList: null,
       specialistList: null,
     };
   },
   async mounted() {
-    axios
-      .get("/api/admin")
-      .then((response) => (this.doctorList = response.data));
-
     await axios
-      .get('/api/admin')
+      .get('/api/specialistList')
       .then((response) => (this.specialistList = response.data));
-
-    this.getSpeciaList();
   },
   methods: {
-    getSpeciaList() {
-      let specialist = this.specialistList;
-
-      let specialNew = [] 
-      specialist.forEach((e) => {
-        if (!specialNew.includes(e)) {
-          specialNew.push(e)
-        }
-      });
-      console.log(specialNew)
+    getDoctors(specialization) {
+    axios
+      .get('/api/admin')
+      .then((response) => (this.doctorList = response.data.filter(x => x.specialist === specialization && x.schedule != '')));
     },
+    async pickDoctor(id, schedule) {
+      store.commit("userID", id)
+      store.commit("doctorSched", schedule)
+      store.state.statusAvailability = true
+      await this.$router.push('/registration')
+    }
   },
 };
 </script>
 
 <style>
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.doctorContainer {
+  width: 50% !important;
+}
+@media (max-width: 991.98px) {
+  .doctorContainer {
+    width: 97% !important;
+  }
+}
 </style>
