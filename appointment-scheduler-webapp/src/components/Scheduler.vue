1d716fff0790f2b46adba9fe83a438fa3b57b9a5
Leindfraust <rozer223@gmail.com>
2021-07-17 23:05:46 +0800

    added timeStart and timeEnd, improved layout, improved attributes.
    attribute datepickers have their own separate of getting a date and will not be syncing. timeStart and timeEnd variables will automatically convert into Local Time String for better front end.

--- appointment-scheduler-webapp/src/components/Scheduler.vue
+++ appointment-scheduler-webapp/src/components/Scheduler.vue
@@ -1,70 +1,127 @@
 <template>
-  <section class="hero is-primary is-fullheight">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <v-calendar
-          v-model="date"
-          :attributes="attributes"
-          @dayclick="modalUp"
-          :timezone="timezone"
-        />
-        <br />
-        <div class="modal" :class="{ 'is-active': isActive }">
-          <div class="modal-background"></div>
-          <div class="modal-content">
-            <v-date-picker v-model="date" mode="time" :timezone="timezone" />
+  <div class="columns">
+    <div class="column is-1">
+      <aside class="menu">
+        <p class="menu-label">Hello, {{ alias }}</p>
+        <figure class="media block">
+            <p class="image is-64x64">
+              <img
+                :src="require(`../assets/doctors/${alias}` + '.jpg')"
+              />
+            </p>
+          </figure>
+        <button @click="logout" class="button is-danger" type="button">
+          Logout
+        </button>
+        <ul class="menu-list">
+          <li>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input"
+                  type="text"
+                  v-model="searchBar"
+                  placeholder="Search..."
+                />
+              </div>
+            </div>
+          </li>
+          <li><a @click="routeHome">Home</a></li>
+          <li><a>Profile</a></li>
+          <li><a @click="openSchedule">Schedule</a></li>
+        </ul>
+      </aside>
+    </div>
+    <div class="column">
+      <section class="hero is-primary is-fullheight">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <v-calendar
+              v-model="date"
+              :attributes="attributes"
+              @dayclick="modalUp"
+              :timezone="timezone"
+            />
+            <br />
+            <div class="modal" :class="{ 'is-active': isActive }">
+              <div class="modal-background"></div>
+              <div class="modal-content">
+                <v-date-picker
+                  v-model="timeStart"
+                  mode="time"
+                  :timezone="timezone"
+                /><br>
+                <v-date-picker
+                  v-model="timeEnd"
+                  mode="time"
+                  :timezone="timezone"
+                />
+                <div class="block">
+                  <button class="button" type="button" @click="addSched">
+                    Confirm
+                  </button>
+                </div>
+              </div>
+              <button
+                class="modal-close is-large"
+                aria-label="close"
+                @click="modalClose"
+              ></button>
+            </div>
+            <br />
             <div class="block">
-              <button class="button" type="button" @click="addSched">
-                Confirm
+              <button class="button" @click="uploadSched" type="button">
+                Update schedules
               </button>
             </div>
-          </div>
-          <button
-            class="modal-close is-large"
-            aria-label="close"
-            @click="modalClose"
-          ></button>
-        </div>
-        <br>
-        <div class="block">
-          <button class="button" @click="uploadSched" type="button">Update schedules</button>
-        </div>
 
-        <h1 class="title">Schedule List</h1>
-        <div class="block card" v-for="(schedules, index) in days" :key="schedules.id">
-          <div class="card-content">
-            <div class="content">
-              <p class="subtitle has-text-black"> {{ index + 1 }}</p>
-              <p class="sbuttile"> {{ schedules.date }} </p>
-              <p class="subtitle has-text-black"> {{ schedules.time }} </p>
+            <h1 class="title">Schedule List</h1>
+            <div
+              class="block card"
+              v-for="(schedules, index) in days"
+              :key="schedules.id"
+            >
+              <div class="card-content">
+                <div class="content">
+                  <p class="subtitle has-text-black">{{ index + 1 }}</p>
+                  <p class="sbuttile">{{ schedules.date }}</p>
+                  <p class="subtitle has-text-black">{{ schedules.timeStart }} - {{ schedules.timeEnd }}</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import store from "../store";
 
 export default {
   name: "Scheduler",
   data() {
     return {
-      userID: '',
+      alias: store.state.alias,
+      userID: "",
       date: new Date(),
+      timeStart: new Date(),
+      timeEnd: new Date(),
       timezone: "",
       days: [],
       checkServer: null,
       isActive: false,
     };
   },
-  async mounted(){
-    await axios.get('/session/user')
-    .then(response => this.days = response.data.schedule)
-    await axios.get('/session/user')
-    .then(response => this.userID = response.data._id)
+  async mounted() {
+    await axios
+      .get("/session/user")
+      .then((response) => (this.days = response.data.schedule));
+    await axios
+      .get("/session/user")
+      .then((response) => (this.userID = response.data._id));
   },
   computed: {
     dates() {
@@ -82,7 +139,8 @@ export default {
       this.days.push({
         id: day.id,
         date: day.date.toLocaleDateString(),
-        time: this.date.toLocaleTimeString()
+        timeStart: this.timeStart.toLocaleTimeString(),
+        timeEnd: this.timeEnd.toLocaleTimeString()
       });
     },
     modalUp(day) {
@@ -95,22 +153,33 @@ export default {
       this.day = day;
     },
     addSched(day) {
-        day = this.day;
-        this.onDayClick(day);
-        this.isActive = !this.isActive;
+      day = this.day;
+      this.onDayClick(day);
+      this.isActive = !this.isActive;
     },
     modalClose() {
       this.isActive = !this.isActive;
     },
     async uploadSched() {
       await axios.put(`/api/admin/${this.userID}`, {
-        schedule: this.days
+        schedule: this.days,
       });
-      await axios.put('/session/user', {
-        schedule: this.days
-      })
+      await axios.put("/session/user", {
+        schedule: this.days,
+      });
       location.reload();
-    }
+    },
+    async logout() {
+      store.commit("alias", null);
+      await axios.delete("/session/user");
+      await this.$router.push("/login");
+    },
+    async routeHome() {
+      await this.$router.push(`/admin/user/${this.alias}`)
+    },
+    async openSchedule(){
+      await this.$router.push(`/admin/user/${this.alias}/schedule`)
+    },
   },
 };
 </script>
