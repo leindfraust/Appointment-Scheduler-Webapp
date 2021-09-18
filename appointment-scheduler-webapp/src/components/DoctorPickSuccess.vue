91ad80453ddff18a108dbbb67a389017cb7d99be
Leindfraust <rozer223@gmail.com>
2021-09-19 00:19:06 +0800

    added verification when successfully appointed
    Displays client details when they are finished in their registration form.

---
+++ appointment-scheduler-webapp/src/components/DoctorPickSuccess.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section is-large">
+    <h1 class="subtitle has-text-centered">
+      You have successfuly appointed to <b>{{ client.doctor }}</b> at
+      {{ client.schedule.date }}, your <b>priority number</b> is
+      <b>{{ client.priorityNum }}</b>
+    </h1>
+    <hr />
+    <h1 class="title">Patient Details </h1><br>
+    <h1 class="subtitle"><b>First Name:</b> {{ client.firstName }}</h1>
+    <h1 class="subtitle"><b>Last Name:</b> {{ client.lastName }}</h1>
+    <h1 class="subtitle"><b>Contact Number:</b> {{ client.contactNum }}</h1>
+    <h1 class="subtitle"><b>Birthday:</b> {{ client.birthDay }}</h1>
+    <h1 class="subtitle"><b>Additional comments:</b> {{ client.comments }}</h1>
+  </section>
+</template>
+
+<script>
+import store from "../store";
+export default {
+  name: "pickDoctorSuccess",
+  data() {
+    return {
+      client: store.state.patientDetails,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
