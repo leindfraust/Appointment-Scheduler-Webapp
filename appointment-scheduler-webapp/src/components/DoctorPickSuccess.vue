b65bebfc74663aada241d847fef6ae684d5b4ccc
Leindfraust <rozer223@gmail.com>
2021-09-20 10:01:29 +0800

    changed client variable to patient variable for the sake of variable naming consistency

--- appointment-scheduler-webapp/src/components/DoctorPickSuccess.vue
+++ appointment-scheduler-webapp/src/components/DoctorPickSuccess.vue
@@ -1,17 +1,18 @@
 <template>
   <section class="section is-large">
     <h1 class="subtitle has-text-centered">
-      You have successfuly appointed to <b>{{ client.doctor }}</b> at
-      {{ client.schedule.date }}, your <b>priority number</b> is
-      <b>{{ client.priorityNum }}</b>
+      You have been successfuly appointed to <b>{{ patient.doctor }}</b> at
+      <b>{{ patient.schedule.date }}</b>, <b>{{patient.schedule.timeStart}}</b> - <b>{{patient.schedule.timeEnd}}</b>.
     </h1>
+    <h1 class="subtitle has-text-centered">Your <b>priority number</b> is
+      <b>{{ patient.priorityNum }}</b></h1>
     <hr />
     <h1 class="title">Patient Details </h1><br>
-    <h1 class="subtitle"><b>First Name:</b> {{ client.firstName }}</h1>
-    <h1 class="subtitle"><b>Last Name:</b> {{ client.lastName }}</h1>
-    <h1 class="subtitle"><b>Contact Number:</b> {{ client.contactNum }}</h1>
-    <h1 class="subtitle"><b>Birthday:</b> {{ client.birthDay }}</h1>
-    <h1 class="subtitle"><b>Additional comments:</b> {{ client.comments }}</h1>
+    <h1 class="subtitle"><b>First Name:</b> {{ patient.firstName }}</h1>
+    <h1 class="subtitle"><b>Last Name:</b> {{ patient.lastName }}</h1>
+    <h1 class="subtitle"><b>Contact Number:</b> {{ patient.contactNum }}</h1>
+    <h1 class="subtitle"><b>Birthday:</b> {{ patient.birthDay }}</h1>
+    <h1 class="subtitle"><b>Additional comments:</b> {{ patient.comments }}</h1>
   </section>
 </template>
 
@@ -21,7 +22,7 @@ export default {
   name: "pickDoctorSuccess",
   data() {
     return {
-      client: store.state.patientDetails,
+      patient: store.state.patientDetails,
     };
   },
 };
