21b1d8b9c68e34703767ac3e08aadd6bb737ab53
Leindfraust <rozer223@gmail.com>
2021-09-30 10:08:31 +0800

    Added for new password creation

--- appointment-scheduler-webapp/src/components/Profile.vue
+++ appointment-scheduler-webapp/src/components/Profile.vue
@@ -4,51 +4,126 @@
       <aside class="menu">
         <p class="menu-label">Hello, {{ alias }}</p>
         <figure class="media block">
-          <p class="image is-64x64">
-            <img :src="require(`../assets/doctors/${alias}` + '.jpg')" />
-          </p>
+          <span class="image is-128x128" v-html="profileImg"></span>
         </figure>
-        <button @click="logout" class="button is-danger" type="button">
-          Logout
-        </button>
+        <button @click="logout" class="button is-danger" type="button">Logout</button>
         <ul class="menu-list">
-          <li><a @click="routeHome">Home</a></li>
-          <li><a @click="profile">Profile</a></li>
-          <li><a @click="openSchedule">Schedule</a></li>
+          <li>
+            <a @click="routeHome">Home</a>
+          </li>
+          <li>
+            <a @click="profile">Profile</a>
+          </li>
+          <li>
+            <a @click="openSchedule">Schedule</a>
+          </li>
         </ul>
       </aside>
     </div>
-    <div class="column">
+    <div class="column" style="margin-top: 6%">
       <div class="container is-fluid">
-        <img
-          id="profile-img"
-          :src="require(`../assets/doctors/${alias}` + '.jpg')"
-        />
-        <form
-        id="formUpload"
-          action="/api/imgUpload"
-          method="post"
-          enctype="multipart/form-data"
-          style="margin: auto; width: 30%"
-        >
-          <input type="hidden" name="alias" :value="(alias)">
-          <div class="columns">
-            <div class="column is-8">
-              <input class="input" type="file" name="imgFile" required />
-            </div>
-            <div class="column is-1">
-              <button type="submit" @click="onSubmit" class="button is-primary">Change profile picture</button>
-            </div>
+        <div class="columns is-gapless">
+          <div class="column is-6">
+            <span id="profile-img" v-html="profileImg"></span>
+            <form
+              id="formUpload"
+              action="/api/imgUpload"
+              method="post"
+              enctype="multipart/form-data"
+              style="margin: auto; width: 50%"
+            >
+              <input type="hidden" name="alias" :value="(alias)" />
+              <div class="columns">
+                <div class="column is-8">
+                  <input class="input" type="file" name="imgFile" required />
+                </div>
+                <div class="column is-1">
+                  <button
+                    type="submit"
+                    value="Upload"
+                    class="button is-primary"
+                  >Change profile picture</button>
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
-        <br>
-        <form>
-          <label class="label"> <h1 class="subtitle"><b>Full Name:</b> </h1> </label>
-          <input class="input" size="5" type="text" v-model="fullname" style="width: 33%">
-          <br>
-          <br>
-          <button class="button is-danger" type="button" @click="updateFullName"> Save changes </button>
-        </form>
+          <br />
+          <div class="column" style="margin-top: 2%">
+            <form>
+              <label class="label">
+                <h1 class="subtitle">
+                  <b>Full Name:</b>
+                </h1>
+              </label>
+              <input class="input" size="5" type="text" v-model="fullname" style="width: 50%" />
+              <br />
+              <br />
+              <button class="button is-danger" type="button" @click="updateInfo">Save changes</button>
+              <h1
+                v-if="infoValidate"
+                class="subtitle has-text-success"
+                style="margin-top: 5%"
+              >{{ infoValidateMessage }}</h1>
+              <hr />
+              <label class="label">
+                <h1 class="subtitle">
+                  <b>Create new password:</b>
+                </h1>
+              </label>
+              <h1
+                v-if="passwordValidate"
+                class="subtitle has-text-danger"
+                style="margin-top: 5%"
+              >{{ passwordValidateMessage }}</h1>
+              <input
+                class="input"
+                size="5"
+                type="password"
+                id="passwordOne"
+                v-model="password"
+                style="width: 50%"
+              />
+              <br />
+              <label class="checkbox">
+                <input type="checkbox" @click="showPassOne" />
+                Show password
+              </label>
+              <br />
+              <br />
+              <label class="label">
+                <h1 class="subtitle">
+                  <b>Repeat Password:</b>
+                </h1>
+              </label>
+              <h1
+                v-if="passwordValidate"
+                class="subtitle has-text-danger"
+                style="margin-top: 5%"
+              >{{ passwordValidateMessage }}</h1>
+              <input
+                class="input"
+                size="5"
+                type="password"
+                id="passwordTwo"
+                v-model="passwordRepeat"
+                style="width: 50%"
+              />
+              <br />
+              <label class="checkbox">
+                <input type="checkbox" @click="showPassTwo" />
+                Show password
+              </label>
+              <br />
+              <br />
+              <button class="button is-danger" type="button" @click="updatePassword">Save changes</button>
+              <h1
+                v-if="!passwordValidate"
+                class="subtitle has-text-success"
+                style="margin-top: 5%"
+              >{{ passwordValidateMessage }}</h1>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -57,6 +132,7 @@
 <script>
 import store from "../store";
 import axios from "axios";
+import cld from '../cloudinary'
 
 export default {
   name: "userProfile",
@@ -64,7 +140,14 @@ export default {
     return {
       alias: store.state.alias,
       fullname: null,
-      id: null
+      password: null,
+      passwordRepeat: null,
+      passwordValidateMessage: null,
+      passwordValidate: null,
+      infoValidate: null,
+      infoValidateMessage: null,
+      id: null,
+      profileImg: store.state.profileImg
     };
   },
   async mounted() {
@@ -76,18 +159,62 @@ export default {
       .then((response) => (this.id = response.data._id));
   },
   methods: {
-    async onSubmit(e) {
-      if(e )
-      await setTimeout(() => location.reload(), 1000)
-    },
-    async updateFullName() {
+    async updateInfo() {
       await axios.put(`/api/admin/${this.id}`, {
-        name: this.fullname
+        name: this.fullname,
       })
       await axios.put("/session/user", {
-        fullname: this.fullname
+        fullname: this.fullname,
       })
-      location.reload();
+      await axios
+        .get("/session/user")
+        .then((response) => (this.fullname = response.data.fullname));
+      await axios
+        .get("/session/user")
+        .then((response) => (this.id = response.data._id));
+
+      this.infoValidate = true
+      this.infoValidateMessage = "changes saved successfully"
+    },
+    async updatePassword() {
+      if (this.password != null && this.passwordRepeat != null && this.password === this.passwordRepeat) {
+        await axios.put(`/api/admin/${this.id}`, {
+          password: this.password
+        })
+        await axios.put("/session/user", {
+          password: this.password
+        })
+        this.passwordValidate = false
+        this.passwordValidateMessage = "password changed successfully"
+      } else {
+        if (this.password == null && this.passwordRepeat == null) {
+          this.passwordValidate = true;
+          this.passwordValidateMessage = "empty password"
+          this.password = null
+          this.passwordRepeat = null
+        } else {
+          this.passwordValidate = true;
+          this.passwordValidateMessage = "password do not match"
+          this.password = null
+          this.passwordRepeat = null
+        }
+      }
+    },
+    showPassOne() {
+      let passwordToggle = document.getElementById("passwordOne");
+      if (passwordToggle.type === "password") {
+        passwordToggle.type = "text";
+      } else {
+        passwordToggle.type = "password";
+      }
+    },
+    showPassTwo() {
+      let passwordToggle = document.getElementById("passwordTwo");
+      if (passwordToggle.type === "password") {
+        passwordToggle.type = "text";
+      } else {
+        passwordToggle.type = "password";
+      }
     },
     async logout() {
       store.commit("alias", null);
@@ -111,7 +238,7 @@ export default {
 #profile-img {
   display: block;
   margin: auto;
-  width: 30%;
+  width: 70%;
   padding: 40px;
 }
 </style>
