89cc2327fd210bb43feaf1daf135f5a55152d5c3
Leindfraust <rozer223@gmail.com>
2021-10-08 08:47:32 +0800

    Now supports socket.io with a simple chat

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const session = require('express-session');
 const formidable = require('formidable');
+const cors = require('cors');
+const morgan = require('morgan');
 const util = require('util')
 const fs = require('fs');
 const MongoStore = require('connect-mongo');
@@ -13,16 +15,22 @@ const {
     PORT,
     mongoUri
 } = require('./config');
-
+const server = app.listen(PORT, () => {
+    console.log(`listening to ${PORT}`);
+});
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:8080" || "http://192.168.1.11:8080"
+    }
+});
 //schemas
 const specializationRoute = require('./routes/api/specializationList')
 const appointmentListRoute = require('./routes/api/appointmentList')
 const adminRoute = require('./routes/api/adminList')
-const sess = require('./sessions/users/user')
+const sess = require('./sessions/models/admin')
+const user = require('./routes/api/user')
 
-const cors = require('cors');
-const morgan = require('morgan');
-
+//app usages
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
@@ -46,6 +54,41 @@ cloudinary.config({
     api_secret: 'y397TRyKmeYukzShGtUysWR4MFo'
 });
 
+//io middlewares
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username;
+    if (!username) {
+        return next(new Error("invalid username"));
+    }
+    socket.username = username;
+    next();
+});
+
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+    socket.on('message', (msg, user) => {
+        io.emit('chat message', msg, user);
+    });
+
+    const users = [];
+    for (let [id, socket] of io.of("/").sockets) {
+        users.push({
+            userID: id,
+            username: socket.username,
+            connected: socket.connected
+        });
+    }
+    socket.emit("users", users);
+
+    socket.broadcast.emit("user connected", {
+        userID: socket.id,
+        username: socket.username,
+        connected: socket.connected
+    });
+});
+
 //use sessions
 app.use(session({
     secret: 'leindfraust',
@@ -63,10 +106,11 @@ app.use(session({
 app.use('/api/specialistList', specializationRoute)
 app.use('/api/appointmentList', appointmentListRoute)
 app.use('/api/admin', adminRoute)
-app.use('/session/user', sess)
+app.use('/api/user', user)
+app.use('/session/admin', sess)
 
-//doctor image upload
-app.post('/api/imgUpload',async function(req, res, next) {
+//doctor image change profile picture upload
+app.post('/api/imgUpload', async function (req, res, next) {
 
     const form = await new formidable()
     form.parse(req, (err, fields, files) => {
@@ -78,18 +122,43 @@ app.post('/api/imgUpload',async function(req, res, next) {
             public_id: fields.alias,
             folder: "assets/doctors/",
             overwrite: true,
-            invalidate: true, 
+            invalidate: true,
             format: "jpg"
-        }, function (error, result){console.log(result,error); });
+        }, function (error, result) {
+            console.log(result, error);
+        });
     });
-    setTimeout(() => {res.status(200).redirect('/imgUploadSuccess')}, 5000)
+    setTimeout(() => {
+        res.status(200).redirect('/imgUploadSuccess')
+    }, 5000)
 });
+//doctor signup image upload
+app.post('/api/imgUploadAdmin', async function (req, res, next) {
+
+    const form = await new formidable()
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        cloudinary.v2.uploader.upload(files.imgFile.path, {
+            public_id: fields.alias,
+            folder: "assets/doctors/",
+            overwrite: true,
+            invalidate: true,
+            format: "jpg"
+        }, function (error, result) {
+            console.log(result, error);
+        });
+    });
+    setTimeout(() => {
+        res.status(200).redirect('/imgUploadSuccessAdmin')
+    }, 5000)
+});
+
+//serve dist in node local server
 app.get('/', (req, res) => {
 
     res.sendFile(path.join(__dirname, 'appointment-scheduler-webapp/dist/index.html'))
 
-});
-
-app.listen(PORT, () => {
-    console.log(`listening to ${PORT}`);
 });
