48bdfee2bb070b8f6df72f466a19ed74ac4a24d7
Leindfraust <rozer223@gmail.com>
2021-11-03 14:51:24 +0800

    Create AdminMenu.vue

---
+++ appointment-scheduler-webapp/src/components/AdminMenu.vue
@@ -0,0 +1,60 @@
+<template>
+    <aside class="menu">
+        <p class="menu-label">Hello, {{ alias }}</p>
+        <figure class="media block">
+            <span class="image is-128x128" v-html="profileImg"></span>
+        </figure>
+        <button @click="logout" class="button is-danger" type="button">Logout</button>
+        <ul class="menu-list">
+            <li>
+                <a @click="routeHome">Home</a>
+            </li>
+            <li>
+                <a @click="inbox">Inbox</a>
+            </li>
+            <li>
+                <a @click="profile">Profile</a>
+            </li>
+            <li>
+                <a @click="openSchedule">Schedule</a>
+            </li>
+        </ul>
+    </aside>
+</template>
+<script>
+import store from '../store'
+import axios from 'axios'
+import cld from '../cloudinary'
+
+export default {
+    name: "AdminMenu",
+    data() {
+        return {
+            alias: store.state.alias,
+            profileImg: store.state.profileImg
+        }
+    },
+    methods: {
+        async logout() {
+            store.commit("alias", null);
+            await axios.delete("/session/admin");
+            await this.$router.push("/login");
+        },
+        async routeHome() {
+            await this.$router.push(`/admin/user/${this.alias}`);
+        },
+        async profile() {
+            await this.$router.push(`/admin/user/${this.alias}/profile`);
+        },
+        async openSchedule() {
+            await this.$router.push(`/admin/user/${this.alias}/schedule`);
+        },
+        inbox() {
+            this.$router.push('/chatnow')
+            this.$store.commit("userType", "doctor")
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
