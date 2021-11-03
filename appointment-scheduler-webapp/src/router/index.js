2640cc40c730202a5d2eba64a0893f3d6d610d1f
Leindfraust <rozer223@gmail.com>
2021-11-03 14:57:36 +0800

    added routers for patient users and edited paths, meta for patient users

--- appointment-scheduler-webapp/src/router/index.js
+++ appointment-scheduler-webapp/src/router/index.js
@@ -3,42 +3,25 @@ import {
     createWebHistory
 } from 'vue-router'
 import store from '../store'
-import Home from '../views/Home.vue'
-import DoctorList from '../components/DoctorList.vue'
-import Registration from '../views/Registration.vue'
-import LoginAdmin from '../views/LoginAdmin.vue'
-import Admin from '../components/Admin.vue'
-import SignUpAdmin from '../views/SignUpAdmin.vue'
-import Scheduler from '../components/Scheduler.vue'
-import userProfile from '../components/Profile.vue'
-import DoctorPickSuccess from '../components/DoctorPickSuccess.vue'
-import ImageUploadSuccess from '../views/ImageUploadSuccess'
+import Home from '../views/Public/Home.vue'
+import DoctorList from '../views/Public/DoctorList.vue'
+import RegForm from '../views/Public/RegForm.vue'
+import LoginAdmin from '../views/PortalAdmin/LoginAdmin.vue'
+import Admin from '../views/PortalAdmin/Admin.vue'
+import SignUpAdmin from '../views/PortalAdmin/SignUpAdmin.vue'
+import Scheduler from '../views/PortalAdmin/Scheduler.vue'
+import AdminProfile from '../views/PortalAdmin/Profile.vue'
+import DoctorPickSuccess from '../views/PortalSuccessConfirmations/DoctorPickSuccess.vue'
+import ImageUploadSuccess from '../views/PortalSuccessConfirmations/ImageUploadSuccess'
+import UserSignUp from '../views/Public/UserSignUp.vue'
+import UserLogin from '../views/Public/UserLogin.vue'
+import User from '../views/Public/User.vue'
+import DoctorChat from '../views/Public/DoctorChat.vue'
 
     const routes = [{
         path: '/',
         name: 'Home',
         component: Home,
-    },
-    {
-        path: '/doctors',
-        name: 'DoctorList',
-        component: DoctorList,
-    },
-    {
-        path: '/success',
-        name: 'pickDoctorSuccess',
-        component: DoctorPickSuccess,
-        meta: {
-            requireSuccessPickDoctor: true
-        },
-    },
-    {
-        path: '/registration',
-        name: 'Registration',
-        component: Registration,
-        meta: {
-            requireProcess: true
-        },
     },
     {
         path: '/login',
@@ -68,8 +51,8 @@ import ImageUploadSuccess from '../views/ImageUploadSuccess'
     },
     {
         path: '/admin/user/:user/profile',
-        name: 'userProfile',
-        component: userProfile,
+        name: 'AdminProfile',
+        component: AdminProfile,
         meta: {
             requiresAuth: true
         },
@@ -81,7 +64,57 @@ import ImageUploadSuccess from '../views/ImageUploadSuccess'
         meta: {
             requireImgUploadSuccess: true
         }
-    }
+    },
+    {
+        path: '/user/signup',
+        name: 'UserSignup',
+        component: UserSignUp
+    },
+    {
+        path: '/user/login',
+        name: 'UserLogin',
+        component: UserLogin
+    },
+    {
+        path: '/user/:user/',
+        name: 'User',
+        component: User,
+        meta: {
+            requiresAuthPatient: true
+        }
+    },
+    {
+        path: '/chatnow',
+        name: 'DoctorChat',
+        component: DoctorChat,
+        meta: {
+            requiresAuthUsers: true
+        }
+    },
+    {
+        path: '/user/:user/doctors',
+        name: 'DoctorList',
+        component: DoctorList,
+         meta: {
+            requiresAuthPatient: true
+        } 
+    },
+    {
+        path: '/success',
+        name: 'pickDoctorSuccess',
+        component: DoctorPickSuccess,
+        meta: {
+            requireSuccessPickDoctor: true
+        },
+    },
+    {
+        path: '/user/:user/registration',
+        name: 'RegForm',
+        component: RegForm,
+        meta: {
+            requireProcess: true
+        },
+    },
 ]
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
@@ -93,7 +126,7 @@ router.beforeEach((to, from, next) => {
         if (store.state.statusAvailability) {
             return next();
         } else {
-            return next('/doctors');
+            return next('/user/:user/doctors');
         }
     }
     next();
@@ -115,7 +148,7 @@ router.beforeEach((to, from, next) => {
         if (store.state.userID) {
             return next();
         } else {
-            return next('/registration');
+            return next('/user/:user/registration');
         }
     }
     next();
@@ -133,11 +166,33 @@ router.beforeEach((to, from, next) => {
 });
 
 router.beforeEach((to, from, next) => {
+    if (to.matched.some(route => route.meta.requiresAuthPatient)) {
+        if (store.state.patientUsername) {
+            return next();
+        } else {
+            return next('/user/login');
+        }
+    }
+    next();
+});
+
+router.beforeEach((to, from, next) => {
     if (to.matched.some(route => route.meta.requireProcess)) {
         if (store.state.statusAvailability) {
             return next();
         } else {
-            return next('/doctors');
+            return next('/user/:user/doctors');
+        }
+    }
+    next();
+}); 
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(route => route.meta.requiresAuthUsers)) {
+        if (store.state.patientUsername || store.state.alias) {
+            return next();
+        } else {
+            return next('/');
         }
     }
     next();
