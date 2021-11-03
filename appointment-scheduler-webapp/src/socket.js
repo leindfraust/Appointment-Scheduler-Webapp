f324a024bc33d7c9b3eee55745137deff6a0dbef
Leindfraust <rozer223@gmail.com>
2021-11-03 15:22:18 +0800

    update

--- appointment-scheduler-webapp/src/socket.js
+++ appointment-scheduler-webapp/src/socket.js
@@ -1,8 +1,8 @@
 import {
     io
 } from "socket.io-client"
-//const URL = "http://localhost:5000"
-const socket = io({
+const URL = "http://localhost:5000"
+const socket = io(URL,{
     autoConnect: false
 });
 
