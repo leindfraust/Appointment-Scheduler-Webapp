40568aac78c0d661a21cb98ac831e7a186f9473c
Leindfraust <rozer223@gmail.com>
2021-11-25 23:24:38 +0800

    dynamic layout according to circumstances, sorted dates

--- appointment-scheduler-webapp/src/views/Public/RegForm.vue
+++ appointment-scheduler-webapp/src/views/Public/RegForm.vue
@@ -1,98 +1,136 @@
 <template>
-  <section class="hero is-info is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">Registration Form</h1>
-        <form class="field customField animate__animated animate__fadeInLeft">
-          <label class="label">First Name</label>
-          <div class="control">
-            <input
-              class="input"
-              type="text"
-              v-model="firstName"
-              placeholder="First Name"
-              required
-            />
-          </div>
-          <label class="label">Last Name</label>
-          <div class="control">
-            <input
-              class="input"
-              type="text"
-              v-model="lastName"
-              placeholder="Last Name"
-              required
-            />
-          </div>
-          <label class="label">Contact Number:</label>
-          <div class="block control">
-            <input
-              class="input"
-              type="number"
-              v-model="contactNum"
-              placeholder="Contact Number"
-              required
-            />
-          </div>
-          <label class="label">Additional comments</label>
-          <textarea
-            class="textarea"
-            v-model="comments"
-            placeholder="Optional"
-          ></textarea>
-          <label class="label">Birthday</label>
-          <div class="control">
-            <v-date-picker class="block" v-model="birthDay">
-              <template v-slot="{ inputValue, togglePopover }">
-                <div class="is-block">
-                  <button
-                    type="button"
-                    class="button is-primary"
-                    style="margin: auto"
-                    @click="togglePopover()"
-                  >
-                    Select date
-                  </button>
-                  <input
-                    :value="inputValue"
-                    class="input"
-                    style="width: 10%"
-                    readonly
-                  />
-                </div>
-              </template>
-            </v-date-picker>
-          </div>
-          <br />
-          <h1 class="subtitle has-text-black has-text-weight-bold">
-            Pick your prefered schedule
-          </h1>
-          <div
-            class="block card"
-            v-for="schedules in doctorSched"
-            :key="schedules.id"
-          >
+  <section class="section" style="background-color: whitesmoke;">
+    <div class="modal" :class="{ 'is-active': isSchedLoading }">
+      <div class="modal-background"></div>
+      <div class="modal-content" style="overflow: hidden">
+        <div class="loader" style="margin: auto;"></div>
+      </div>
+    </div>
+    <div class="container is-fluid">
+      <div class="tabs is-centered">
+        <ul>
+          <li :class="{ 'is-active': isActiveTabOne }">
+            <a style="pointer-events: none;">Basic Details</a>
+          </li>
+          <li :class="{ 'is-active': isActiveTabTwo }">
+            <a style="pointer-events: none;">Schedules</a>
+          </li>
+        </ul>
+      </div>
+      <section
+        class="section animate__animated animate__fadeInLeft"
+        style="width: 40%; margin: auto; background-color: white; border-radius: 5px; box-shadow: 5px 10px rgb(221, 221, 221);"
+      >
+        <form class="field">
+          <div v-if="!basicDetailsDone">
+            <span class="has-text-danger">All fields are required.*</span>
+            <label class="label">First name</label>
             <div class="control">
-              <label class="radio">
-                <input
-                  type="radio"
-                  name="schedule"
-                  @click="pickSched(schedules)"
-                />
-                <div class="content">
-                  <p class="sbuttile">{{ schedules.date }}</p>
-                  <p class="subtitle has-text-black">
-                    {{ schedules.timeStart }} - {{ schedules.timeEnd }}
-                  </p>
-                </div></label
-              >
+              <input
+                class="input"
+                type="text"
+                :value="firstName"
+                placeholder="First Name"
+                required
+                disabled
+              />
+            </div>
+            <label class="label">Last Name</label>
+            <div class="control">
+              <input class="input" type="text" v-model="lastName" placeholder="Last Name" required disabled/>
+            </div>
+            <label class="label">Contact Number:</label>
+            <div class="block control">
+              <input
+                class="input"
+                type="number"
+                v-model="contactNum"
+                placeholder="Contact Number"
+                required
+              />
+            </div>
+            <label class="label">What are you feeling?</label>
+            <textarea
+              class="textarea"
+              v-model="comments"
+              placeholder="Your symptoms, general well being etc..."
+              required
+            ></textarea>
+            <label class="label">Birthday</label>
+            <div class="control">
+              <v-date-picker class="block" v-model="birthDay">
+                <template v-slot="{ inputValue, togglePopover }">
+                  <div class="is-block">
+                    <a style="margin: auto" @click="togglePopover()">
+                      <input style="cursor: pointer;" :value="inputValue" class="input" readonly />
+                    </a>
+                  </div>
+                </template>
+              </v-date-picker>
+            </div>
+            <br />
+            <div class="block has-text-centered">
+              <button
+                type="button"
+                class="button is-success"
+                :disabled="firstName == '' || lastName == '' || birthDay == null || contactNum == '' || comments == ''"
+                @click="proceedSched"
+              >Proceed</button>
             </div>
           </div>
-          <button type="button" class="button is-primary" @click="appoint">
-            Submit
-          </button>
         </form>
-      </div>
+        <br />
+        <div v-if="basicDetailsDone">
+          <form class="field">
+            <h1 class="subtitle has-text-black has-text-weight-bold">Pick your prefered schedule:</h1>
+            <div
+              v-for="(schedules, index) in doctorSched.filter(x => new Date(x.date).getTime() > new Date().getTime()).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())"
+              :key="schedules.id"
+              style="width: 75%; margin:auto"
+            >
+              <hr />
+              <div class="controls">
+                <label class="radio">
+                  <input
+                    type="radio"
+                    class="radioSched"
+                    name="schedule"
+                    @click="pickSched(index, schedules)"
+                  />
+                  <span
+                    class="subtitle has-text-black"
+                  >{{ new Date(schedules.date).toDateString() }}</span>
+                  <br />
+                  <span class="has-text-black">{{ schedules.timeStart }} - {{ schedules.timeEnd }}</span>
+                </label>
+                <a style="pointer-events: none;" class="button statusSched">
+                  <span v-if="schedAvailability" class="has-text-success">Available</span>
+                  <span v-else class="has-text-danger">Unavailable</span>
+                </a>
+              </div>
+              <br />
+            </div>
+            <br />
+            <br />
+            <div class="block has-text-centered">
+              <span>
+                <button
+                  class="button is-danger"
+                  type="button"
+                  style="margin-right: 5%"
+                  @click="goBack"
+                >Go Back</button>
+                <button
+                  type="button"
+                  class="button is-primary"
+                  :disabled="schedule == null"
+                  @click="appoint"
+                >Submit appoitnment</button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </section>
     </div>
   </section>
 </template>
@@ -105,27 +143,40 @@ export default {
   name: "RegForm",
   data() {
     return {
+      checkPatientRecord: null,
+      patient: null,
       doctorSched: store.state.doctorSched,
       doctor: store.state.userID,
       firstName: null,
       lastName: null,
-      birthDay: new Date(),
+      birthDay: null,
       comments: null,
       contactNum: null,
       schedule: null,
       priorityNum: null,
+      patientsAppointed: null,
       doctorDetails: null,
+      basicDetailsDone: false,
+      isActiveTabOne: true,
+      isActiveTabTwo: false,
+      isSchedLoading: false,
+      schedAvailability: false
     };
   },
-  mounted() {
-    axios
+  async mounted() {
+    await axios
       .get("/api/admin")
       .then(
         (response) =>
-          (this.doctorDetails = response.data.find(
-            (x) => x._id === this.doctor
-          ))
+        (this.doctorDetails = response.data.find(
+          (x) => x._id === this.doctor
+        ))
       );
+    await axios.get('/session/patient').then(response => this.patient = response.data)
+    this.firstName = await this.patient.firstName
+    this.lastName = await this.patient.lastName
+    console.log(await this.patient)
+    this.checkPatientRecord = await this.doctorDetails.patients.find(x => x.patient === this.patient._id)
   },
   methods: {
     async appoint() {
@@ -133,24 +184,25 @@ export default {
         .get("/api/appointmentList")
         .then(
           (response) =>
-            (this.priorityNum =
-              response.data.filter(
-                (e) =>
-                  e.schedule[0].date === this.schedule.date &&
-                  e.doctor === this.doctor
-              ).length + 1)
+          (this.priorityNum =
+            response.data.filter(
+              (e) =>
+                e.schedule[0].date === this.schedule.date &&
+                e.doctor === this.doctor
+            ).length + 1)
         );
-      if (
-        this.firstName != null &&
-        this.lastName != null &&
-        this.contactNum != null
-      ) {
+      if (typeof this.checkPatientRecord === 'undefined' || !this.checkPatientRecord) {
+        await axios.post('/api/patientUpdate', {
+          doctorID: this.doctor,
+          patientID: this.patient._id,
+          patientFullName: this.firstName + " " + this.lastName
+        });
         await axios.post("/api/appointmentList", {
           doctor: this.doctor,
           firstName: this.firstName,
           lastName: this.lastName,
           contactNum: this.contactNum,
-          birthDay: this.birthDay.toLocaleDateString(),
+          birthDay: this.birthDay.toDateString(),
           comments: this.comments,
           schedule: this.schedule,
           priorityNum: this.priorityNum,
@@ -160,52 +212,119 @@ export default {
           firstName: this.firstName,
           lastName: this.lastName,
           contactNum: this.contactNum,
-          birthDay: this.birthDay.toLocaleDateString(),
+          birthDay: this.birthDay.toDateString(),
           comments: this.comments,
           schedule: this.schedule,
           priorityNum: this.priorityNum,
         };
         store.commit("patientDetails", patientDetails);
         await this.$router.push("/success");
+      } else {
+        await axios.post("/api/appointmentList", {
+          doctor: this.doctor,
+          firstName: this.firstName,
+          lastName: this.lastName,
+          contactNum: this.contactNum,
+          birthDay: this.birthDay.toDateString(),
+          comments: this.comments,
+          schedule: this.schedule,
+          priorityNum: this.priorityNum,
+        });
+        let patientDetails = {
+          doctor: this.doctorDetails.name,
+          firstName: this.firstName,
+          lastName: this.lastName,
+          contactNum: this.contactNum,
+          birthDay: this.birthDay.toDateString(),
+          comments: this.comments,
+          schedule: this.schedule,
+          priorityNum: this.priorityNum,
+        };
+        store.commit("statusAvailability", false)
+        store.commit("patientDetails", patientDetails);
+        await this.$router.push("/success");
       }
     },
-    pickSched(sched) {
-      this.schedule = sched;
+    proceedSched() {
+      this.isActiveTabOne = false
+      this.isActiveTabTwo = true
+      this.basicDetailsDone = true
+    },
+    goBack() {
+      this.isActiveTabOne = true
+      this.isActiveTabTwo = false
+      this.basicDetailsDone = false
+    },
+    async pickSched(e, sched) {
+      this.isSchedLoading = true
+      let radio = document.getElementsByClassName('radioSched');
+      let statusSched = document.getElementsByClassName('statusSched')
+      for (let i = 0; i < statusSched.length; i++) {
+        statusSched[i].style.display = 'none'
+      }
+      this.schedule = await sched;
+      await axios
+        .get("/api/appointmentList")
+        .then(
+          (response) =>
+          (this.patientsAppointed =
+            response.data.filter(
+              (e) =>
+                e.schedule[0].date === this.schedule.date &&
+                e.doctor === this.doctor
+            ).length));
+      //check how many appointed patients in regards to the appointment limit set by the doctor
+      if (await this.patientsAppointed < this.schedule.appointmentLimit) {
+        this.isSchedLoading = false
+        this.schedAvailability = true
+        statusSched[e].style.display = 'block'
+      } else {
+        this.isSchedLoading = false
+        this.schedAvailability = false
+        this.schedule = null
+        radio[e].checked = false
+        radio[e].disabled = true
+        statusSched[e].style.display = 'block'
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-.customField {
-  width: 100%;
-  margin: auto;
-  padding: 100px;
-  background-color: whitesmoke;
-  border-radius: 15px;
+.statusSched {
+  display: none;
 }
-input,
-textarea,
-.card {
-  width: 35%;
+.loader {
+  border: 7px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 7px solid gray;
+  border-bottom: 7px solid gray;
+  width: 60px;
+  height: 60px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
 }
-.card {
-  padding: 50px;
+
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
 }
-button {
-  margin-top: 15px;
-  margin-right: 60%;
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 @media (max-width: 991.98px) {
-  .customField {
-    width: 100% !important;
-    padding: 50px;
-  }
-  input,
-  textarea,
-  .card,
-  .is-block,
-  button {
+  .section {
     width: 100% !important;
   }
 }
