4e3b92cf19e0c6cbdaff7c3968556780aa2da0a5
Leindfraust <rozer223@gmail.com>
2022-02-17 13:14:57 +0800

    Update ImageUploadSuccessAdmin.vue

--- appointment-scheduler-webapp/src/views/PortalSuccessConfirmations/ImageUploadSuccessAdmin.vue
+++ appointment-scheduler-webapp/src/views/PortalSuccessConfirmations/ImageUploadSuccessAdmin.vue
@@ -1,7 +1,25 @@
 <template>
+    <div class="container" style="margin-top: 20%">
+        <h1
+            class="title has-text-centered"
+        >Your account has been successfully created, please login onto your account and setup your schedule to start.</h1>
+        <br />
+        <h1 class="subtitle has-text-centered">Redirecting browser in 15 seconds.</h1>
+        <h1 class="subtitle has-text-centered">
+            If the browser does not reload automatically, please click
+            <a href="/admin/login">here</a>
+        </h1>
+    </div>
 </template>
+
 <script>
 export default {
-    name: "imageUploadSuccessAdmin"
+    name: "imageUploadSuccessAdmin",
+    created() {
+        setTimeout(() => { window.location.href = "/admin/login" }, 15000)
+    }
 }
 </script>
+
+<style>
+</style>
