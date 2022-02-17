dbf72eb91444110c7a294f882a3c9475af8fd8c3
Leindfraust <rozer223@gmail.com>
2022-02-17 13:24:34 +0800

    AdminMenu now is mobile friendly

--- appointment-scheduler-webapp/src/components/AdminMenu.vue
+++ appointment-scheduler-webapp/src/components/AdminMenu.vue
@@ -1,24 +1,62 @@
 <template>
-    <aside class="menu">
-        <p class="menu-label">Hello, {{ alias }}</p>
-        <figure class="media block">
-            <span class="image is-128x128" v-html="profileImg"></span>
-        </figure>
-        <button @click="logout" class="button is-danger" type="button">Logout</button>
+    <nav class="navbar is-hidden-desktop">
+        <div class="navbar-brand">
+            <a class="navbar-item">
+                <label class="label">{{ alias }}</label>
+            </a>
+            <a
+                role="button"
+                class="navbar-burger"
+                :class="{ 'is-active': !isActive }"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbar"
+                @click="menuNav"
+            >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+    </nav>
+    <aside
+        class="menu"
+        :class="{ 'is-hidden-mobile': isActive, 'fixedMenu': isActive }"
+        style="background-color: white; padding: 20px; box-shadow: 5px 10px rgba(240, 240, 240, 0.966);"
+    >
+        <div class="has-text-centered">
+            <label class="label" v-if="isActive">{{ alias }}</label>
+            <span class="image is-128x128" style="margin: auto;" v-html="profileImg"></span>
+        </div>
+        <br />
         <ul class="menu-list">
+            <p class="menu-label">General</p>
             <li>
-                <a @click="routeHome">Home</a>
+                <a @click="routeHome">Appointments</a>
             </li>
             <li>
-                <a @click="patientLogs">Patient logs</a>
-            </li>
-            <li>
-                <a @click="profile">Profile</a>
+                <a @click="patientLogs">Patients</a>
             </li>
             <li>
                 <a @click="openSchedule">Schedule</a>
             </li>
+            <p class="menu-label">Verification and Information</p>
+            <li>
+                <a @click="profile">Profile</a>
+            </li>
+            <li>
+                <a @click="openSecurity">Security</a>
+            </li>
+            <p class="menu-label">Support</p>
+            <li>
+                <a @click="openFAQs">FAQ</a>
+            </li>
+            <li>
+                <a @click="openContact">Contact</a>
+            </li>
         </ul>
+        <hr />
+        <button @click="logout" class="button is-danger" type="button">Logout</button>
     </aside>
 </template>
 <script>
@@ -31,7 +69,8 @@ export default {
     data() {
         return {
             alias: store.state.alias,
-            profileImg: store.state.profileImg
+            profileImg: store.state.profileImg,
+            isActive: true
         }
     },
     methods: {
@@ -49,11 +88,20 @@ export default {
         async patientLogs() {
             await this.$router.push(`/admin/user/${this.alias}/patients`)
         },
+        async openSecurity() {
+            await this.$router.push(`/admin/user/${this.alias}/security`)
+        },
         async openSchedule() {
             await this.$router.push(`/admin/user/${this.alias}/schedule`);
         },
+        menuNav() {
+            this.isActive = !this.isActive
+        }
     }
 }
 </script>
 <style scoped>
+.fixedMenu {
+    position: fixed
+}
 </style>
