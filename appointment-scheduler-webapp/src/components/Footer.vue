ecdb9a30bfddda162c526caf9de6427760a7a97e
Leindfraust <rozer223@gmail.com>
2022-02-17 13:24:16 +0800

    Create Footer.vue

---
+++ appointment-scheduler-webapp/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="footer" style="background-color: white;">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Developed and Designed</strong> by
+                <strong>Ronan and Timay</strong>.
+            </p>
+            <p>
+                <strong>Researched thoroughly</strong> by
+                <strong>Gabriel and Cloyde.</strong>
+            </p>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: "Footer"
+}
+</script>
