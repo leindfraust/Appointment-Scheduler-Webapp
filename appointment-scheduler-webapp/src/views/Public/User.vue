33fde291e18875cb291aaf19c51313ca4b191312
Leindfraust <rozer223@gmail.com>
2022-02-17 12:55:49 +0800

    added hospitalfinder and removed non functioning code

--- appointment-scheduler-webapp/src/views/Public/User.vue
+++ appointment-scheduler-webapp/src/views/Public/User.vue
@@ -1,49 +1,45 @@
 <template>
-<NavigationTabVue/>
-<section class="section" >
-        <div class="box" style="margin: auto; width:100%;"><p class="subtitle">{{patient}}</p>
-        <figure class="media block">
-           <img id="imgAvatar" :src="imgPreUrl + patient + svgExtUrl">
-        </figure>
-        <button class="button is-info" @click="doctors">Get started</button>
+    <NavigationTabVue />
+    <section class="section">
+        <div class="container has-text-centered">
+            <HospitalFinderVue />
         </div>
     </section>
-
 </template>
 <script>
 import axios from 'axios'
 import NavigationTabVue from '../../components/NavigationTab.vue'
+import socket from '../../socket'
+import HospitalFinderVue from '../../components/HospitalFinder.vue'
 
 export default {
     name: 'User',
     components: {
-        NavigationTabVue
+        NavigationTabVue,
+        HospitalFinderVue
     },
-    data(){
+    data() {
         return {
-            patient: this.$store.state.patientUsername,
             imgPreUrl: "https://avatars.dicebear.com/api/micah/",
-            svgExtUrl: ".svg"
+            svgExtUrl: ".svg",
+            notifications: []
         }
     },
     methods: {
-        doctors() {
-            this.$router.push(`/user/${this.patient}/doctors`)
-        },
-        inbox(){
+        inbox() {
             this.$router.push('/chatnow')
             this.$store.commit("userType", "patient")
         },
-        async logout(){
+        async logout() {
             this.$store.commit("patientUsername", null);
             await axios.delete("/session/patient");
             await this.$router.push("/user/login");
-        }
+        },
     }
 }
 </script>
 <style scoped>
 #imgAvatar {
-    width: 10%
+    width: 10%;
 }
 </style>
