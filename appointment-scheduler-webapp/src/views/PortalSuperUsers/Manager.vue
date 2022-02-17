9d627c83463a73ae0669e9218a36ea3a8b713770
Leindfraust <rozer223@gmail.com>
2022-02-17 13:14:22 +0800

    added managermenu,

--- appointment-scheduler-webapp/src/views/PortalSuperUsers/Manager.vue
+++ appointment-scheduler-webapp/src/views/PortalSuperUsers/Manager.vue
@@ -1,85 +1,105 @@
 <template>
-<section class="section">
-<h1 class="title">{{managerHospital.hospital}}</h1>
-<button class="button is-danger" @click="logout">Logout</button>
-    <div class="field">
-        <div class="control" style="width: 33%;">
-            <label class="label">Search: </label>
-            <input class="input" type="text" v-model="searchBar" placeholder="Search...">
+    <div class="columns">
+        <div class="column is-2">
+            <ManagerMenuVue/>
         </div>
-    </div>
-    <div class="table-container">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th class="has-text-black-ter">Controls</th>
-                    <th class="has-text-black-ter">No.</th>
-                    <th class="has-text-black-ter">Alias</th>
-                    <th class="has-text-black-ter">Full Name</th>
-                    <th class="has-text-black-ter">Specialist</th>
-                    <th class="has-text-black-ter">Gmail</th>
-                    <th class="has-text-black-ter">Schedules</th>
-                </tr>
-            </thead>
-            <tbody v-for="(doctor, index) in doctorAccountsIndexed" :key="doctor._id">
-                <tr>
-                    <button
-                        class="dropdown-item button has-text-info"
-                        type="button"
-                        @click="doctorRecoverEmail(doctor._id, doctor.gmail)"
-                    >Recover</button>
-                    <button
-                        class="button dropdown-item has-text-danger"
-                        type="button"
-                        @click="doctorRemove(doctor._id)"
-                    >Remove</button>
-                    <td class="has-text-black-ter">{{ index + 1 }}</td>
-                    <td class="has-text-black-ter">{{ doctor.alias }}</td>
-                    <td class="has-text-black-ter">{{ doctor.name }}</td>
-                    <td class="has-text-black-ter">{{ doctor.specialist }}</td>
-                    <td class="has-text-black-ter">{{ doctor.gmail }}</td>
-                    <td
-                        class="has-text-black-ter"
-                        v-for="doctorSchedule in doctor.schedule"
-                    >{{ doctorSchedule.date }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="modal" :class="{'is-active': isActiveModal}">
-        <div class="modal-background"></div>
-        <div class="modal-content">
-            <div class="box">
+        <div class="column" style="overflow: auto;">
+        <div class="notification is-light is-danger" v-if="hospitalStatus.status == 'Inactive'">The account is not activated yet and will not be displayed on hospital searches, complete the hospital's <router-link :to="`/user/manager/${this.managerHospital.hospital}/profile`" class="has-text-link">profile</router-link> to activate the account.</div>
+            <section class="section">
+                <h1 class="title">{{ managerHospital.hospital }}</h1>
                 <div class="field">
-                    <div class="control">
-                        <input class="input" type="text" v-model="email">
+                    <div class="control" style="width: 33%;">
+                        <label class="label">Search:</label>
+                        <input
+                            class="input"
+                            type="text"
+                            v-model="searchBar"
+                            placeholder="Search..."
+                        />
                     </div>
-                    <button class="button is-success" type="button" @click="updateData">Confirm</button>
                 </div>
-            </div>
-             <button class="modal-close is-large" aria-label="close" @click="closeModal"></button>
+                <div class="table-container">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="has-text-black-ter">Controls</th>
+                                <th class="has-text-black-ter">No.</th>
+                                <th class="has-text-black-ter">Alias</th>
+                                <th class="has-text-black-ter">Full Name</th>
+                                <th class="has-text-black-ter">Specialist</th>
+                                <th class="has-text-black-ter">Gmail</th>
+                                <th class="has-text-black-ter">Schedules</th>
+                            </tr>
+                        </thead>
+                        <tbody v-for="(doctor, index) in doctorAccountsIndexed" :key="doctor._id">
+                            <tr>
+                                <button
+                                    class="dropdown-item button has-text-info"
+                                    type="button"
+                                    @click="doctorRecoverEmail(doctor._id, doctor.gmail)"
+                                >Recover</button>
+                                <button
+                                    class="button dropdown-item has-text-danger"
+                                    type="button"
+                                    @click="doctorRemove(doctor._id)"
+                                >Remove</button>
+                                <td class="has-text-black-ter">{{ index + 1 }}</td>
+                                <td class="has-text-black-ter">{{ doctor.alias }}</td>
+                                <td class="has-text-black-ter">{{ doctor.name }}</td>
+                                <td class="has-text-black-ter">{{ doctor.specialist.toString() }}</td>
+                                <td class="has-text-black-ter">{{ doctor.gmail }}</td>
+                                <td
+                                    class="has-text-black-ter"
+                                    v-for="doctorSchedule in doctor.schedule"
+                                >{{ doctorSchedule.date }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal" :class="{ 'is-active': isActiveModal }">
+                    <div class="modal-background"></div>
+                    <div class="modal-content">
+                        <div class="box">
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input" type="text" v-model="email" />
+                                </div>
+                                <button
+                                    class="button is-success"
+                                    type="button"
+                                    @click="updateData"
+                                >Confirm</button>
+                            </div>
+                        </div>
+                        <button class="modal-close is-large" aria-label="close" @click="closeModal"></button>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
-    </section>
 </template>
 <script>
 import axios from 'axios'
+import ManagerMenuVue from '../../components/ManagerMenu.vue'
 
 export default {
     name: "Manager",
+    components: {
+        ManagerMenuVue
+    },
     computed: {
         doctorAccountsIndexed() {
-            return this.doctorAccounts.filter(x => {return x.name.toLowerCase().includes(this.searchBar.toLowerCase()) || x.specialist.toLowerCase().includes(this.searchBar.toLowerCase())})
+            return this.doctorAccounts.filter(x => { return x.name.toLowerCase().includes(this.searchBar.toLowerCase()) || x.alias.toLowerCase().includes(this.searchBar.toLowerCase()) })
         },
     },
     async mounted() {
         await axios.get('/session/manager').then(response => this.managerHospital = response.data)
+        await axios.get('/api/manager').then(response => this.hospitalStatus = response.data.find(x => x._id == this.managerHospital._id))
         await axios.get('/api/admin').then(response => this.doctorAccounts = response.data.filter(x => x.hospitalOrigin.find(x => x.hospital === this.managerHospital.hospital)))
-        console.log(await this.doctorAccounts)
-        console.log(this.managerHospital.hospital)
     },
     data() {
         return {
+            hospitalStatus: '',
             managerHospital: '',
             searchBar: '',
             isActiveModal: false,
@@ -89,19 +109,19 @@ export default {
         }
     },
     methods: {
-        doctorRecoverEmail(id, email){
+        doctorRecoverEmail(id, email) {
             this.id = id
             this.email = email
             this.isActiveModal = true
         },
-        async updateData(){
+        async updateData() {
             await axios.put(`/api/admin/${this.id}`, {
                 gmail: this.email
             });
             await axios.get('/api/admin').then(response => this.doctorAccounts = response.data)
             this.isActiveModal = !this.isActiveModal
         },
-        async doctorRemove(){
+        async doctorRemove() {
             await axios.post('/api/doctorPullHospital', {
                 doctorID: this.id,
                 hospital: this.managerHospital.hospital
@@ -109,11 +129,6 @@ export default {
         },
         closeModal() {
             this.isActiveModal = false
-        },
-        async logout(){
-            await axios.delete('/session/manager');
-            await this.$store.commit("managerHospital", null);
-            await this.$router.push("/user/manager/login");
         },
     }
 }
