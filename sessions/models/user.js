78e01192ea410304587290b0d0fa024e37d995b5
Leindfraust <rozer223@gmail.com>
2022-03-23 07:20:07 +0800

    refactor: added email session for patient account

--- sessions/models/user.js
+++ sessions/models/user.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const app = express()
+const parser = app.use(express.json())
 
 router.get('/', (req, res) => {
     res.send(req.session)
 });
 
-router.post('/', (req, res) => {
+router.post('/', parser, (req, res) => {
     req.session._id = req.body._id
     req.session.name = req.body.name
+    req.session.email = req.body.email
     req.session.username = req.body.username
     req.session.province = req.body.province
     req.session.city = req.body.city
@@ -20,9 +23,10 @@ router.post('/', (req, res) => {
     res.status(200).send('OK');
 });
 
-router.put('/', (req, res) => {
+router.put('/', parser, (req, res) => {
     req.session.alias = req.body.alias || req.session.alias
     req.session.name = req.body.name || req.session.name
+    req.session.email = req.body.email || req.session.email
     req.session.username = req.body.username || req.session.username
     req.session.province = req.body.province || req.session.province
     req.session.city = req.body.city || req.session.city
