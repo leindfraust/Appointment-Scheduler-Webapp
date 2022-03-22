387dd19c0c59d1fcee518e6b96dc03d8a3b7d759
Leindfraust <rozer223@gmail.com>
2022-03-23 07:13:45 +0800

    refactor: delete unnecessary code and targets Home component

--- appointment-scheduler-webapp/src/views/Public/User.vue
+++ appointment-scheduler-webapp/src/views/Public/User.vue
@@ -1,45 +1,8 @@
 <template>
-    <NavigationTabVue />
-    <section class="section">
-        <div class="container has-text-centered">
-            <HospitalFinderVue />
-        </div>
-    </section>
+    <Home/>
 </template>
-<script>
-import axios from 'axios'
-import NavigationTabVue from '../../components/NavigationTab.vue'
-import socket from '../../socket'
-import HospitalFinderVue from '../../components/HospitalFinder.vue'
-
-export default {
-    name: 'User',
-    components: {
-        NavigationTabVue,
-        HospitalFinderVue
-    },
-    data() {
-        return {
-            imgPreUrl: "https://avatars.dicebear.com/api/micah/",
-            svgExtUrl: ".svg",
-            notifications: []
-        }
-    },
-    methods: {
-        inbox() {
-            this.$router.push('/chatnow')
-            this.$store.commit("userType", "patient")
-        },
-        async logout() {
-            this.$store.commit("patientUsername", null);
-            await axios.delete("/session/patient");
-            await this.$router.push("/user/login");
-        },
-    }
-}
+<script setup>
+import Home from '../../views/Public/Home'
 </script>
 <style scoped>
-#imgAvatar {
-    width: 10%;
-}
 </style>
