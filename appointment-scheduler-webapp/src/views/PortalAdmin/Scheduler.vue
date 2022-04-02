f1ace8575c1385cbd3964d3d45470d2d0f7420a9
Leindfraust <rozer223@gmail.com>
2022-04-02 14:19:54 +0800

    feat: v-calendar now has optional viewpoints, and UI changes

--- appointment-scheduler-webapp/src/views/PortalAdmin/Scheduler.vue
+++ appointment-scheduler-webapp/src/views/PortalAdmin/Scheduler.vue
@@ -1,122 +1,129 @@
 <template>
-  <div class="columns" style="height: 100vh; background-color: whitesmoke">
-    <div class="column is-2" style="background-color: whitesmoke;">
-      <AdminMenu />
-    </div>
-    <div class="column" style="background-color: whitesmoke;">
-      <section class="section">
-        <h1 class="title">SETUP SCHEDULES</h1>
-        <!--modal date input-->
-        <div class="modal" :class="{ 'is-active': isActive }">
-          <div class="modal-background"></div>
-          <div class="modal-content">
-            <div class="card">
-              <div class="card-content">
-                <div class="content has-text-centered">
-                  <div class="control block">
-                    <label class="label">Prefix:</label>
-                    <input
-                      class="input"
-                      type="text"
-                      maxlength="10"
-                      v-model="prefix"
-                      style="width: 33%;"
-                      placeholder="e.g. ROOM305"
+  <div style="overflow-x: hidden; height: 100vh; background-color: whitesmoke;">
+    <div class="columns">
+      <div class="column is-2">
+        <AdminMenu />
+      </div>
+      <div class="column" style="background-color: whitesmoke;">
+        <section class="section">
+          <h1 class="title">SETUP SCHEDULES</h1>
+          <!--modal date input-->
+          <div class="modal" :class="{ 'is-active': isActive }">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+              <div class="card">
+                <div class="card-content">
+                  <div class="content has-text-centered">
+                    <div class="control block">
+                      <label class="label">Prefix:</label>
+                      <input
+                        class="input"
+                        type="text"
+                        maxlength="10"
+                        v-model="prefix"
+                        style="width: 33%;"
+                        placeholder="e.g. ROOM305"
+                      />
+                    </div>
+                    <label class="label">Time start:</label>
+                    <v-date-picker
+                      v-model="timeStart"
+                      mode="time"
+                      :timezone="timezone"
+                      class="block"
                     />
-                  </div>
-                  <label class="label">Time start:</label>
-                  <v-date-picker
-                    v-model="timeStart"
-                    mode="time"
-                    :timezone="timezone"
-                    class="block"
-                  />
-                  <br />
-                  <label class="label">Time end:</label>
-                  <v-date-picker v-model="timeEnd" mode="time" :timezone="timezone" class="block" />
-                  <div class="control block">
-                    <label class="label">Appointment limit:</label>
-                    <input
-                      class="input"
-                      type="number"
-                      v-model="appointmentLimits"
-                      style="width: 33%;"
+                    <br />
+                    <label class="label">Time end:</label>
+                    <v-date-picker
+                      v-model="timeEnd"
+                      mode="time"
+                      :timezone="timezone"
+                      class="block"
                     />
-                  </div>
-                  <div class="block">
-                    <button
-                      class="button is-primary"
-                      type="button"
-                      @click="addSched"
-                      :disabled="prefix == ''"
-                    >Confirm</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <button class="modal-close is-large" aria-label="close" @click="modalClose"></button>
-        </div>
-        <!--modal loader-->
-        <div class="modal" :class="{ 'is-active': loading }">
-          <div class="modal-background"></div>
-          <div class="modal-content loader"></div>
-        </div>
-        <div class="columns">
-          <div class="column">
-            <div
-              class="notification is-success has-text-centered"
-              v-if="uploadSchedSuccess"
-            >Schedules Added/Updated!</div>
-            <v-calendar
-              :columns="$screens({ default: 1, lg: 2 })"
-              :rows="$screens({ default: 1, lg: 2 })"
-              :is-expanded="$screens({ default: true, lg: false })"
-              v-model="date"
-              :min-date="new Date()"
-              :attributes="attributes"
-              @dayclick="modalUp"
-              :timezone="timezone"
-            />
-          </div>
-          <div class="column">
-            <div class="container">
-              <h1 class="subtitle">Ongoing Schedules:</h1>
-              <div
-                class="columns is-multiline"
-                style="overflow: scroll; max-height: 30em"
-                v-if="Object.keys(daysIndexed).length !== 0"
-              >
-                <div
-                  class="column is-6"
-                  v-for="(schedules, index) in daysIndexed"
-                  :key="schedules.id"
-                >
-                  <div class="block card">
-                    <div class="card-content">
-                      <div class="content">
-                        <p
-                          class="title is-4 has-text-black has-text-left"
-                        >{{ new Date(schedules.date).toDateString() }}</p>
-                        <p class="has-text-black">Prefix: {{ schedules.prefix }}</p>
-                        <p
-                          class="has-text-black"
-                        >{{ schedules.timeStart }} - {{ schedules.timeEnd }}</p>
-                        <p
-                          class="has-text-black"
-                        >Appointment limit: {{ schedules.appointmentLimit }}</p>
-                      </div>
+                    <div class="control block">
+                      <label class="label">Appointment limit:</label>
+                      <input
+                        class="input"
+                        type="number"
+                        v-model="appointmentLimits"
+                        style="width: 33%;"
+                      />
+                    </div>
+                    <div class="block">
+                      <button
+                        class="button is-primary"
+                        type="button"
+                        @click="addSched"
+                        :disabled="prefix == ''"
+                      >Confirm</button>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="notification is-warning" v-else>
-                <h1 class="title is-5">No ongoing schedules.</h1>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="modalClose"></button>
+          </div>
+          <!--modal loader-->
+          <div class="modal" :class="{ 'is-active': loading }">
+            <div class="modal-background"></div>
+            <div class="modal-content loader"></div>
+          </div>
+          <div class="columns">
+            <div class="column">
+              <div
+                class="notification is-success has-text-centered"
+                v-if="uploadSchedSuccess"
+              >Schedules Added/Updated!</div>
+              <v-calendar
+                :columns="$screens({ default: 1, lg: 2 })"
+                :rows="$screens({ default: 1, lg: 2 })"
+                :is-expanded="$screens({ default: true, lg: false })"
+                v-model="date"
+                :min-date="new Date()"
+                :attributes="attributes"
+                @dayclick="modalUp"
+                :timezone="timezone"
+              />
+            </div>
+            <div class="column">
+              <div class="container">
+                <h1 class="subtitle">Ongoing Schedules:</h1>
+                <div
+                  class="columns is-multiline"
+                  style="overflow: scroll; max-height: 30em"
+                  v-if="Object.keys(daysIndexed).length !== 0"
+                >
+                  <div
+                    class="column is-6"
+                    v-for="(schedules, index) in daysIndexed"
+                    :key="schedules.id"
+                  >
+                    <div class="block card">
+                      <div class="card-content">
+                        <div class="content">
+                          <p
+                            class="title is-4 has-text-black has-text-left"
+                          >{{ new Date(schedules.date).toDateString() }}</p>
+                          <p class="has-text-black">Prefix: {{ schedules.prefix }}</p>
+                          <p
+                            class="has-text-black"
+                          >{{ schedules.timeStart }} - {{ schedules.timeEnd }}</p>
+                          <p
+                            class="has-text-black"
+                          >Appointment limit: {{ schedules.appointmentLimit }}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="notification is-warning" v-else>
+                  <h1 class="title is-5">No ongoing schedules.</h1>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   </div>
 </template>
