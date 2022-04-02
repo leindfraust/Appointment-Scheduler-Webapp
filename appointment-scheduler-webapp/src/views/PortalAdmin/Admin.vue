582bee222b187592af05cbb3bea1ed167c7d67ca
Leindfraust <rozer223@gmail.com>
2022-04-02 14:22:34 +0800

    fix: current day and future appointments now show properly

--- appointment-scheduler-webapp/src/views/PortalAdmin/Admin.vue
+++ appointment-scheduler-webapp/src/views/PortalAdmin/Admin.vue
@@ -1,125 +1,150 @@
 <template>
-  <div class="columns" style="height: 100vh; background-color: whitesmoke">
-    <div class="column is-2" style="background-color: whitesmoke;" >
-      <AdminMenu />
-    </div>
-    <div class="column" style="background-color: whitesmoke;">
-      <section class="section" style="background-color: whitesmoke;">
-      <h1 class="title" >APPOINTMENTS</h1>
-        <div class="container is-widescreen is-fullhd" v-if="Object.keys(appointmentSchedules).length !== 0">
-          <div class="field">
-            <div class="control">
-              <input
-                class="input"
-                type="text"
-                style="width: 50% !important"
-                v-model="searchBar"
-                placeholder="Search..."
-              />
+  <div style="overflow-x: hidden; height: 100vh; background-color: whitesmoke;">
+    <div class="columns">
+      <div class="column is-2">
+        <AdminMenu />
+      </div>
+      <div class="column" style="background-color: whitesmoke;">
+        <section class="section" style="background-color: whitesmoke;">
+          <h1 class="title">APPOINTMENTS</h1>
+          <div
+            class="container is-widescreen is-fullhd"
+            v-if="Object.keys(appointmentSchedules).length !== 0"
+          >
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input"
+                  type="text"
+                  style="width: 50% !important"
+                  v-model="searchBar"
+                  placeholder="Search..."
+                />
+              </div>
             </div>
-          </div>
-          <div class="box" v-for="(appointmentList, index) in appointmentSchedules" :key="index" :style="new Date(index).toDateString() == new Date().toDateString() ? 'box-shadow: rgb(10 10 10 / 10%) 0px 0.5em 1em -0.125em, #485fc7 0px 0px 0px 1px !important;' : ''">
-          <h1 class="subtitle" v-if="new Date(index).toDateString() == new Date().toDateString()"><b>TODAY</b></h1>
-            <h1 class="subtitle has-text-black">Schedule: {{ new Date(index).toDateString() }}</h1>
-            <div class="table-container">
-              <table class="table is-striped is-narrow is-fullwidth is-bordered">
-                <thead>
-                  <tr>
-                    <th class="has-text-black-ter">Controls</th>
-                    <th class="has-text-black-ter">Priority No.</th>
-                    <th class="has-text-black-ter">Hospital Appointed</th>
-                    <th class="has-text-black-ter">First Name</th>
-                    <th class="has-text-black-ter">Last Name</th>
-                    <th class="has-text-black-ter">Contact Number</th>
-                    <th class="has-text-black-ter">Birthday</th>
-                    <th class="has-text-black-ter">Symptoms/Comments</th>
-                  </tr>
-                </thead>
-                <tbody v-for="appointments in appointmentList" :key="appointments._id">
-                  <tr>
-                    <button
-                      class="dropdown-item button has-text-info"
-                      type="button"
-                      @click="
-                        toggleModal(
-                          appointments.firstName,
-                          appointments.lastName,
-                          appointments.emailAdd,
-                          appointments.contactNum,
-                          appointments.birthDay,
-                          appointments._id
-                        )
-                      "
-                    >Edit</button>
-                    <br />
-                    <div class="modal" :class="{ 'is-active': isActiveModal }">
-                      <div class="modal-background"></div>
-                      <div class="modal-content">
-                        <form
-                          class="field customField animate__animated animate__fadeInLeft has-text-centered"
-                        >
-                          <label class="label">First Name</label>
-                          <div class="control">
-                            <input
-                              class="input"
-                              type="text"
-                              v-model="firstName"
-                              placeholder="First Name"
-                              required
-                            />
-                          </div>
-                          <label class="label">Last Name</label>
-                          <div class="control">
-                            <input
-                              class="input"
-                              type="text"
-                              v-model="lastName"
-                              placeholder="Last Name"
-                              required
-                            />
-                          </div>
-                          <label class="label">Contact Number</label>
-                          <div class="control">
-                            <input
-                              class="input"
-                              type="number"
-                              v-model="contactNum"
-                              placeholder="Contact Number"
-                              required
-                            />
-                          </div>
-                          <label class="label">Birthday</label>
-                          <div class="control">
-                            <input
-                              class="input"
-                              type="text"
-                              v-model="birthDay"
-                              placeholder="Birthday"
-                              required
-                            />
-                          </div>
-                          <button class="button is-primary" type="button" @click="updateData">Submit</button>
-                        </form>
+            <div
+              class="box"
+              v-for="(appointmentList, index) in appointmentSchedules"
+              :key="index"
+              :style="new Date(index).toDateString() == new Date().toDateString() ? 'box-shadow: rgb(10 10 10 / 10%) 0px 0.5em 1em -0.125em, #485fc7 0px 0px 0px 1px !important;' : ''"
+            >
+              <h1
+                class="subtitle"
+                v-if="new Date(index).toDateString() == new Date().toDateString()"
+              >
+                <b>TODAY</b>
+              </h1>
+              <h1 class="subtitle has-text-black">Schedule: {{ new Date(index).toDateString() }}</h1>
+              <div class="table-container">
+                <table class="table is-striped is-narrow is-fullwidth is-bordered">
+                  <thead>
+                    <tr>
+                      <th class="has-text-black-ter">Controls</th>
+                      <th class="has-text-black-ter">Priority No.</th>
+                      <th class="has-text-black-ter">Hospital Appointed</th>
+                      <th class="has-text-black-ter">First Name</th>
+                      <th class="has-text-black-ter">Last Name</th>
+                      <th class="has-text-black-ter">Contact Number</th>
+                      <th class="has-text-black-ter">Birthday</th>
+                      <th class="has-text-black-ter">Symptoms/Comments</th>
+                    </tr>
+                  </thead>
+                  <tbody v-for="appointments in appointmentList" :key="appointments._id">
+                    <tr>
+                      <button
+                        class="dropdown-item button has-text-info"
+                        type="button"
+                        @click="
+                          toggleModal(
+                            appointments.firstName,
+                            appointments.lastName,
+                            appointments.emailAdd,
+                            appointments.contactNum,
+                            appointments.birthDay,
+                            appointments._id
+                          )
+                        "
+                      >Edit</button>
+                      <br />
+                      <div class="modal" :class="{ 'is-active': isActiveModal }">
+                        <div class="modal-background"></div>
+                        <div class="modal-content animate__animated animate__fadeInLeft">
+                          <form
+                            class="field customField has-text-centered"
+                          >
+                            <label class="label">First Name</label>
+                            <div class="control">
+                              <input
+                                class="input"
+                                type="text"
+                                v-model="firstName"
+                                placeholder="First Name"
+                                required
+                              />
+                            </div>
+                            <label class="label">Last Name</label>
+                            <div class="control">
+                              <input
+                                class="input"
+                                type="text"
+                                v-model="lastName"
+                                placeholder="Last Name"
+                                required
+                              />
+                            </div>
+                            <label class="label">Contact Number</label>
+                            <div class="control">
+                              <input
+                                class="input"
+                                type="number"
+                                v-model="contactNum"
+                                placeholder="Contact Number"
+                                required
+                              />
+                            </div>
+                            <label class="label">Birthday</label>
+                            <div class="control">
+                              <input
+                                class="input"
+                                type="text"
+                                v-model="birthDay"
+                                placeholder="Birthday"
+                                required
+                              />
+                            </div>
+                            <button
+                              class="button is-primary"
+                              type="button"
+                              @click="updateData"
+                            >Submit</button>
+                          </form>
+                        </div>
+                        <button
+                          class="modal-close is-large"
+                          aria-label="close"
+                          @click="toggleModal"
+                        ></button>
                       </div>
-                      <button class="modal-close is-large" aria-label="close" @click="toggleModal"></button>
-                    </div>
-                    <th class="has-text-black-ter">{{ appointments.priorityNum }}</th>
-                    <th class="has-text-black-ter">{{ appointments.hospital }}</th>
-                    <td class="has-text-black-ter">{{ appointments.firstName }}</td>
-                    <td class="has-text-black-ter">{{ appointments.lastName }}</td>
-                    <td class="has-text-black-ter">{{ appointments.contactNum }}</td>
-                    <td class="has-text-black-ter">{{ appointments.birthDay }}</td>
-                    <td class="has-text-black-ter">{{ appointments.comments }}</td>
-                  </tr>
-                </tbody>
-              </table>
+                      <th class="has-text-black-ter">{{ appointments.priorityNum }}</th>
+                      <th class="has-text-black-ter">{{ appointments.hospital }}</th>
+                      <td class="has-text-black-ter">{{ appointments.firstName }}</td>
+                      <td class="has-text-black-ter">{{ appointments.lastName }}</td>
+                      <td class="has-text-black-ter">{{ appointments.contactNum }}</td>
+                      <td class="has-text-black-ter">{{ appointments.birthDay }}</td>
+                      <td class="has-text-black-ter">{{ appointments.comments }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
-        <section class="section" v-else>
-          <div class="notification is-info"><h1 class="title is-5">No upcoming appointments.</h1></div>
+          <section class="section" v-else>
+            <div class="notification is-info">
+              <h1 class="title is-5">No upcoming appointments.</h1>
+            </div>
+          </section>
         </section>
-      </section>
+      </div>
     </div>
   </div>
 </template>
@@ -160,7 +185,7 @@ export default {
           );
         }).sort((a, b) => {
           return new Date(a.schedule[0].date).getTime() - new Date(b.schedule[0].date).getTime()
-        }).filter(x => { return new Date(x.schedule[0].date).toDateString() <= new Date().toDateString() })
+        }).filter(x => { return new Date(x.schedule[0].date).getTime() >= new Date().getTime() && new Date(x.schedule[0].date).getMonth() >= new Date().getMonth() })
         ,
         "schedule[0].date"
       );
@@ -236,7 +261,7 @@ th {
     width: 300px !important;
   }
   #titleBox {
-    width: 100% !important
+    width: 100% !important;
   }
 }
 </style>
