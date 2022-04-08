1e32032df4be98bc83e55decd14184249d8f6767
Leindfraust <rozer223@gmail.com>
2022-04-09 06:10:41 +0800

    fix: authentication not working properly
    added password checking if mongoose does not find any valid usernames

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const Patient = require('../models/user')
 const Manager = require('../models/manager')
 
 //patient login
-const patientAuth = (async(req, res) => {
+const patientAuth = (async (req, res) => {
     let username = req.body.username
     let password = req.body.password
     let userDetails
@@ -15,23 +15,27 @@ const patientAuth = (async(req, res) => {
         if (err) {
             res.status(500).send(err)
         } else {
-            userDetails = await result
-            bcrypt.compare(await password, result.password, (err, result) => {
-                delete userDetails.password
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    if (result == true) {
-                        let response_client = ({
-                            ...userDetails
-                        }._doc)
-                        delete response_client.password
-                        res.status(200).send(response_client)
+            if (result) {
+                userDetails = await result
+                bcrypt.compare(await password, result.password, (err, result) => {
+                    delete userDetails.password
+                    if (err) {
+                        res.status(500).send(err);
                     } else {
-                        res.status(200).send(result)
+                        if (result == true) {
+                            let response_client = ({
+                                ...userDetails
+                            }._doc)
+                            delete response_client.password
+                            res.status(200).send(response_client)
+                        } else {
+                            res.status(200).send(result)
+                        }
                     }
-                }
-            })
+                })
+            } else {
+                res.status(200).send(false)
+            }
         }
     })
 });
@@ -48,22 +52,27 @@ const doctorAuth = (async (req, res) => {
         if (err) {
             res.status(500).send(err)
         } else {
-            userDetails = await result
-            bcrypt.compare(await password, result.password, (err, result) => {
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    if (result == true) {
-                        let response_client = ({
-                            ...userDetails
-                        }._doc)
-                        delete response_client.password
-                        res.status(200).send(response_client)
+            if (result) {
+                userDetails = await result
+                bcrypt.compare(await password, result.password, (err, result) => {
+                    delete userDetails.password
+                    if (err) {
+                        res.status(500).send(err);
                     } else {
-                        res.status(200).send(result)
+                        if (result == true) {
+                            let response_client = ({
+                                ...userDetails
+                            }._doc)
+                            delete response_client.password
+                            res.status(200).send(response_client)
+                        } else {
+                            res.status(200).send(result)
+                        }
                     }
-                }
-            })
+                })
+            } else {
+                res.status(200).send(false)
+            }
         }
     })
 });
@@ -80,25 +89,34 @@ const managerAuth = (async (req, res) => {
         if (err) {
             res.status(500).send(err)
         } else {
-            userDetails = await result
-            bcrypt.compare(await password, result.password, (err, result) => {
-                if (err) {
-                    console.log(err)
-                    res.status(500).send(err);
-                } else {
-                    if (result == true) {
-                        let response_client = ({
-                            ...userDetails
-                        }._doc)
-                        delete response_client.password
-                        res.status(200).send(response_client)
+            if (result) {
+                userDetails = await result
+                bcrypt.compare(await password, result.password, (err, result) => {
+                    delete userDetails.password
+                    if (err) {
+                        console.log(err)
+                        res.status(500).send(err);
                     } else {
-                        res.status(200).send(result)
+                        if (result == true) {
+                            let response_client = ({
+                                ...userDetails
+                            }._doc)
+                            delete response_client.password
+                            res.status(200).send(response_client)
+                        } else {
+                            res.status(200).send(result)
+                        }
                     }
-                }
-            })
+                })
+            } else {
+                res.status(200).send(false)
+            }
         }
     })
 });
 
-module.exports = {patientAuth, doctorAuth, managerAuth}
+module.exports = {
+    patientAuth,
+    doctorAuth,
+    managerAuth
+}
