181e78749cb1aeb9d61caa6c72d72092a964c5b0
Leindfraust <rozer223@gmail.com>
2022-06-14 07:02:30 +0800

    style: whitespacing

--- client/src/views/Public/ContactSupport.vue
+++ client/src/views/Public/ContactSupport.vue
@@ -55,32 +55,20 @@ function generateTicket() {
         </div>
         <div class="container box" style="width: 33%">
             <div class="notification is-danger" v-if="errMsg && !success">{{ errMsg }}</div>
-            <div
-                class="notification is-success"
-                v-if="success"
-            >Your problem has been reported, your ticket ID is: {{ ticketID }}. Please see your email for further instructions.</div>
+            <div class="notification is-success" v-if="success">Your problem has been reported, your ticket ID is: {{
+                    ticketID
+            }}. Please see your email for further instructions.</div>
             <div v-else>
                 <h1 class="title">Contact Us</h1>
                 <div class="dropdown is-hoverable block">
                     <div class="dropdown-trigger">
-                        <button
-                            class="button"
-                        >{{ ticketType == '' ? 'Select a Problem' : ticketType }}</button>
+                        <button class="button">{{ ticketType == '' ? 'Select a Problem' : ticketType }}</button>
                     </div>
                     <div class="dropdown-menu">
                         <div class="dropdown-content">
-                            <a
-                                class="dropdown-item"
-                                @click="ticketType = 'General Inquiry'"
-                            >General Inquiry</a>
-                            <a
-                                class="dropdown-item"
-                                @click="ticketType = 'Account Problem'"
-                            >Account Problem</a>
-                            <a
-                                class="dropdown-item"
-                                @click="ticketType = 'Report a bug'"
-                            >Report a bug</a>
+                            <a class="dropdown-item" @click="ticketType = 'General Inquiry'">General Inquiry</a>
+                            <a class="dropdown-item" @click="ticketType = 'Account Problem'">Account Problem</a>
+                            <a class="dropdown-item" @click="ticketType = 'Report a bug'">Report a bug</a>
                         </div>
                     </div>
                 </div>
@@ -104,11 +92,9 @@ function generateTicket() {
                 </div>
                 <div class="field has-text-centered">
                     <div class="control">
-                        <button
-                            class="button is-primary"
-                            @click="submitIssue"
-                            :disabled="ticketType == '' || email == '' || title == '' || message == ''"
-                        >Submit Issue</button>
+                        <button class="button is-primary" @click="submitIssue"
+                            :disabled="ticketType == '' || email == '' || title == '' || message == ''">Submit
+                            Issue</button>
                     </div>
                 </div>
             </div>
