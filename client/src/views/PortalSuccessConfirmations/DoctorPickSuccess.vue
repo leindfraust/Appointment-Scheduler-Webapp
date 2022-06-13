518d04cc88e36f3497e95e4c1ec18351c82f79b4
Leindfraust <rozer223@gmail.com>
2022-06-14 07:03:58 +0800

    refactor: added refID

--- client/src/views/PortalSuccessConfirmations/DoctorPickSuccess.vue
+++ client/src/views/PortalSuccessConfirmations/DoctorPickSuccess.vue
@@ -1,12 +1,16 @@
 <template>
-<NavigationTab/>
+  <NavigationTab />
   <section class="section is-medium">
     <h1 class="subtitle has-text-centered">
       You have been successfuly appointed to <b>{{ patient.doctor }}</b> at
-      <b>{{ new Date(patient.schedule.date).toDateString() }}</b>, <b>{{patient.schedule.timeStart}}</b> - <b>{{patient.schedule.timeEnd}}</b> in <b>{{patient.hospital}}</b>.
+      <b>{{ new Date(patient.schedule.date).toDateString() }}</b>, <b>{{ patient.schedule.timeStart }}</b> -
+      <b>{{ patient.schedule.timeEnd }}</b> in <b>{{ patient.hospital }}</b>.
     </h1>
     <h1 class="subtitle has-text-centered">Your <b>priority number</b> is
-      <b>{{ patient.priorityNum }}</b></h1>
+      <b>{{ patient.priorityNum }}</b>
+    </h1>
+    <br />
+    <p class="subtitle is-6 has-text-centered has-text-weight-bold">Reference ID: {{ patient.referenceID }}</p>
     <hr />
     <h1 class="title">Patient Details </h1><br>
     <h1 class="subtitle"><b>First Name:</b> {{ patient.firstName }}</h1>
@@ -21,16 +25,16 @@
 import store from "../../store";
 import NavigationTab from "../../components/NavigationTab.vue";
 export default {
-    name: "pickDoctorSuccess",
-    data() {
-        return {
-            patient: store.state.patientDetails,
-        };
-    },
-    mounted(){
-      store.commit("statusAvailability", false)
-    },
-    components: { NavigationTab }
+  name: "pickDoctorSuccess",
+  data() {
+    return {
+      patient: store.state.patientDetails,
+    };
+  },
+  mounted() {
+    store.commit("statusAvailability", false)
+  },
+  components: { NavigationTab }
 };
 </script>
 
