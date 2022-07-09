43d21c1dd1ec5c50e446934f0f32a68302bd95be
Leindfraust <rozer223@gmail.com>
2022-07-09 15:08:58 +0800

    feat: Doctors can now Confirm/Cancel Appointments

--- client/src/views/PortalDoctor/Doctor.vue
+++ client/src/views/PortalDoctor/Doctor.vue
@@ -37,21 +37,28 @@
                   </thead>
                   <tbody v-for="appointments in appointmentList" :key="appointments._id">
                     <tr>
-                      <button class="dropdown-item button has-text-info" type="button" @click="
-                        toggleModal(
+                      <button class="button has-text-success" type="button" @click="
+                        toggleConfirmModal(
+                          appointments.referenceID,
+                          appointments.doctorName,
+                          appointments.patientID,
                           appointments.firstName,
                           appointments.lastName,
-                          appointments.emailAdd,
                           appointments.contactNum,
                           appointments.birthDay,
                           appointments._id
                         )
-                      ">Edit</button>
+                      " v-if="new Date(index).toDateString() == new Date().toDateString()">Confirm Visitation</button>
+                      <button class="button has-text-danger" type="button" @click="toggleCancelModal(appointments)"
+                        v-else>Cancel Visitation</button>
                       <br />
-                      <div class="modal" :class="{ 'is-active': isActiveModal }">
+
+                      <!--Modal Confirm-->
+                      <div class="modal" :class="{ 'is-active': isActiveModalConfirm }">
                         <div class="modal-background"></div>
                         <div class="modal-content animate__animated animate__fadeInLeft">
-                          <form class="field customField has-text-centered">
+                          <div class="field customField has-text-centered">
+                            <h1 class="title is-4">Confirm Patient Details</h1>
                             <label class="label">First Name</label>
                             <div class="control">
                               <input class="input" type="text" v-model="firstName" placeholder="First Name" required />
@@ -69,11 +76,35 @@
                             <div class="control">
                               <input class="input" type="text" v-model="birthDay" placeholder="Birthday" required />
                             </div>
-                            <button class="button is-primary" type="button" @click="updateData">Submit</button>
-                          </form>
+                            <div class="field">
+                              <label class="label">Upload a prescription(optional)</label>
+                              <div class="control">
+                                <input @change="handleFile($event)" class="input" type="file" />
+                              </div>
+                              <button class="is-hidden" type="submit" value="submit">Submit</button>
+                            </div>
+                            <button class="button is-success" @click="confirmVisitation()">Submit</button>
+                          </div>
                         </div>
-                        <button class="modal-close is-large" aria-label="close" @click="toggleModal"></button>
+                        <button class="modal-close is-large" aria-label="close" @click="cancelModalConfirm"></button>
                       </div>
+
+                      <!--Modal Cancel-->
+                      <div class="modal" :class="{ 'is-active': isActiveModalCancel }">
+                        <div class="modal-background"></div>
+                        <div class="modal-content animate__animated animate__fadeInLeft">
+                          <p class="subtitle is-4 has-text-centered">Do you wish to cancel the appointment? </p>
+                          <p class="subtitle is-6 has-text-centered">The user will be notified of the cancellation via
+                            email and notifications if you wish to proceed.</p>
+                          <p class="help">NOTE: This action cannot be undone.</p>
+                          <div class="buttons is-centered">
+                            <button class="button has-text-success" @click="cancelAppointment">YES</button>
+                            <button class="button has-text-danger" @click="cancelModalCancel">NO</button>
+                          </div>
+                        </div>
+                        <button class="modal-close is-large" aria-label="close" @click="cancelModalCancel"></button>
+                      </div>
+
                       <th class="has-text-black-ter">{{ appointments.referenceID }}</th>
                       <th class="has-text-black-ter">{{ appointments.priorityNum }}</th>
                       <th class="has-text-black-ter">{{ appointments.hospital }}</th>
@@ -104,6 +135,7 @@ import axios from "axios";
 import store from "../../store";
 import _ from 'lodash'
 import DoctorMenu from "../../components/DoctorMenu.vue";
+import socket from '../../socket'
 
 export default {
   name: "DoctorDashboard",
@@ -114,15 +146,20 @@ export default {
     return {
       alias: store.state.alias,
       profileImg: store.state.profileImg,
-      isActiveModal: false,
+      isActiveModalConfirm: false,
+      isActiveModalCancel: false,
       id: null,
+      refID: null,
+      refIDPatient: null,
       firstName: null,
       lastName: null,
       birthDay: null,
       contactNum: null,
+      doctorName: null,
       searchBar: "",
       schedule: null,
       appointmentSched: [],
+      file: null
     };
   },
   computed: {
@@ -137,7 +174,7 @@ export default {
             );
           }).sort((a, b) => {
             return new Date(a.schedule[0].date).getTime() - new Date(b.schedule[0].date).getTime()
-          }).filter(x => { return new Date(x.schedule[0].date).getTime() >= new Date().getTime() && new Date(x.schedule[0].date).getMonth() >= new Date().getMonth() })
+          }).filter(x => { return new Date(x.schedule[0].date).getTime() >= new Date().getTime() && new Date(x.schedule[0].date).getMonth() >= new Date().getMonth() && x.ifPatientVisited == false })
           ,
           "schedule[0].date"
         );
@@ -150,40 +187,98 @@ export default {
     await axios.post('/api/appointmentList/doctors', { id: store.state.doctorID }).then(response => this.appointmentSched = response.data);
   },
   methods: {
-    async updateData() {
-      await axios.put(`/api/appointmentList/${this.id}`, {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        emailAdd: this.emailAddress,
-        contactNum: this.contactNum,
-        birthDay: this.birthDay,
-      });
-      await axios
-        .get("/api/appointmentList")
-        .then(
-          (response) =>
-          (this.appointmentSched = response.data.filter(
-            (x) => x.doctorID === store.state.doctorID
-          ))
-        );
-      this.isActiveModal = !this.isActiveModal;
+    //need to fix
+    async confirmVisitation() {
+      this.generateRefID()
+
+      let titleMsg = 'Appointment visitation confirmed'
+      let noticeMsg = `Your appointment visitation has been confirmed by your doctor Ms/Mr. ${this.doctorName}, reference ID: ${this.refIDPatient}, a digital prescription will be displayed below if your doctor has uploaded one. We hope you well!`
+      socket.emit('message', this.refID, titleMsg, noticeMsg, "Med Search", new Date())
+
+      const formData = new FormData()
+      formData.append('id', this.refID)
+      formData.append('imgFile', this.file)
+
+      try {
+        await axios.post("/api/imgUploadImgMsg", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        });
+        await axios.put(`/api/appointmentList/${this.id}`, {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          contactNum: this.contactNum,
+          birthDay: this.birthDay,
+          ifPatientVisited: true
+        });
+        await axios.post('/api/appointmentList/doctors', { id: store.state.doctorID }).then(response => this.appointmentSched = response.data);
+      } catch (err) {
+        console.log(err)
+      }
+      this.isActiveModalConfirm = false
     },
-    async toggleModal(
+    async cancelAppointment() {
+
+      let titleMsg = 'Your appointment has been cancelled!'
+      let noticeMsg = `Your appointment visitation has been cancelled by your doctor Ms/Mr. ${this.doctorName}, reference ID: ${this.refIDPatient}, this is due to some emergencies or problems in their side. We hope you understand.`
+      socket.emit('message', null, titleMsg, noticeMsg, "Med Search", new Date())
+
+      await axios.delete(`/api/appointmentList/${this.id}`).catch(err => console.log(err))
+      await axios.post('/api/appointmentList/doctors', { id: store.state.doctorID }).then(response => this.appointmentSched = response.data).catch(err => console.log(err));
+
+      this.isActiveModalCancel = false
+    },
+    async toggleConfirmModal(
+      refID,
+      doctorName,
+      patientID,
       firstName,
       lastName,
-      emailAdd,
       contactNum,
       birthDay,
       _id
     ) {
-      this.isActiveModal = !this.isActiveModal;
+      this.isActiveModalConfirm = true
+      this.refIDPatient = refID
+      this.doctorName = doctorName
       this.firstName = firstName;
       this.lastName = lastName;
-      this.emailAddress = emailAdd;
       this.contactNum = contactNum;
       this.birthDay = birthDay;
       this.id = _id;
+      socket.connect()
+      socket.emit('join room', patientID)
     },
+    cancelModalConfirm() {
+      this.isActiveModalConfirm = false
+      socket.disconnect()
+    },
+    async toggleCancelModal(appointment) {
+      this.isActiveModalCancel = true
+      this.refIDPatient = appointment.referenceID
+      this.doctorName = appointment.doctorName
+      this.id = appointment._id
+      socket.connect()
+      socket.emit('join room', appointment.patientID)
+    },
+    cancelModalCancel() {
+      this.isActiveModalCancel = false
+      socket.disconnect()
+    },
+    generateRefID() {
+      let result = '';
+      let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let charactersLength = characters.length;
+      for (let i = 0; i < 12; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+          charactersLength));
+      }
+      this.refID = result
+    },
+    handleFile(e) {
+      this.file = e.target.files[0]
+    }
   },
 };
 </script>
