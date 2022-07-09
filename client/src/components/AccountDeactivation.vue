b096b65989a1222c51fbc3ecbb7427c1e9a4612d
Leindfraust <rozer223@gmail.com>
2022-07-09 15:22:42 +0800

    BREAKING CHANGE: IN DEVELOPMENT

---
+++ client/src/components/AccountDeactivation.vue
@@ -0,0 +1,30 @@
+<script setup>
+import ref from 'vue'
+import axios from 'axios'
+import { useStore } from 'vuex';
+
+const store = useStore();
+const props = defineProps({
+    userType: String
+});
+let activeModal = ref(false);
+
+if (props.userType == 'patient') {
+    await axios.post("/api/patientDeactivate", {
+        id: store.state.patientID
+    });
+}
+</script>
+<template>
+    <a class="subtitle has-text-danger">Deactivate Account</a>
+    <div class="modal" :class="{ 'is-active': activeModal }">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="section">
+                <h1 class="title is-4 has-text-centered">Do you wish to deactivate your account?</h1>
+                <p class="subtitle is-4">All of your data will remain </p>
+            </div>
+        </div>
+        <button class="modal-close is-large"></button>
+    </div>
+</template>
