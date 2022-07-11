552ae932646063a7bae719a201691e533e2f4862
Leindfraust <rozer223@gmail.com>
2022-07-12 07:09:16 +0800

    refactor: automatically switch between dev and prod

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,9 +1,8 @@
 import {
     io
 } from "socket.io-client"
-//const URL = "http://localhost:5000"
-const socket = io({
+const URL = "http://localhost:5000"
+const socket = io(process.env.NODE_ENV === 'development' ? URL : '', {
     autoConnect: false
 });
-
 export default socket;
