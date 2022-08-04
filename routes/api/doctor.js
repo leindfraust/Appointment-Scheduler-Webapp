acfd11816f19c8a5f4afa8502d9ba34afc1a7dcd
Leindfraust <rozer223@gmail.com>
2022-08-04 13:17:15 +0800

    refactor: added email checking and doctorFilter

--- routes/api/doctor.js
+++ routes/api/doctor.js
@@ -2,24 +2,30 @@ const express = require('express');
 const router = express.Router();
 const {
     getDoctors,
+    getDoctorsForFilter,
     pushDoctor,
     updateDoctor,
     deleteDoctor,
     verify_username,
     check_username,
     check_alias,
-    check_registrationCode
+    check_registrationCode,
+    check_email
 } = require('../../controllers/doctorController');
 
 router.post('/check_registrationCode', check_registrationCode)
 
 router.post('/check_alias', check_alias)
 
+router.post('/check_email', check_email)
+
 router.post('/check_username', check_username)
 
 router.post('/verify_username', verify_username)
 
 router.get('/', getDoctors);
+
+router.get('/filteration', getDoctorsForFilter);
 
 router.post('/', pushDoctor);
 
