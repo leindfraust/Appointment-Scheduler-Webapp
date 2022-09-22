510d4771af57939452e07c31d0cccf95da20a2c7
Leindfraust <rozer223@gmail.com>
2022-09-22 23:49:49 +0800

    chore: dist build update
    General changes
    
    -performance improvements(code splitting)
    -UI improvements

---
+++ client/dist/js/597.1ce55cf5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[597,710],{2750:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var u=t(6252),r=t(2262),c=t(6356),s={__name:"UserView",setup(e){return(e,n)=>((0,u.wg)(),(0,u.j4)((0,r.SU)(c["default"])))}};const a=s;var f=a}}]);
+//# sourceMappingURL=597.1ce55cf5.js.map
