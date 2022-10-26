0c56031673b7802384bfb276aa9e261c3c4417ec
Leindfraust <rozer223@gmail.com>
2022-10-26 18:42:41 +0800

    refactor: ignore error in session reload

--- sessions/user.js
+++ sessions/user.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    req.session.reload((err) => {
-        if (err) console.log(err)
+    req.session.reload(() => {
         res.status(200).send(req.session)
     });
 });
