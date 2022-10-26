caedc9d883a696518dcaff0c275ef3421e438db9
Leindfraust <rozer223@gmail.com>
2022-10-26 20:11:49 +0800

    feat: added About page

---
+++ client/src/views/Public/AboutMedicSearch.vue
@@ -0,0 +1,103 @@
+<script setup>
+import NavigationTab from '../../components/NavigationTab.vue';
+import FooterBlock from '../../components/FooterBlock.vue';
+</script>
+<template>
+    <NavigationTab />
+    <section class="section">
+        <h1 class="title">ABOUT</h1>
+        <hr>
+        <div class="content">
+
+            <h2>Introduction</h2>
+
+            <p style="white-space: pre-wrap;">Technology has radically changed our planet and way of life over time.
+                Additionally, senior – friendly
+                technology has produces incredible tools and services that have put helpful information at our
+                fingertips.
+                Smartwatches and smartphones are example of multipurpose device made possible by modern technology.
+                Computers
+                are now more powerful, portable, and faster than ever before. Technology has aided in all of these
+                revolution by
+                making our lives simpler, faster, better, and more enjoyable. With the help of social media platforms
+                and
+                instant messaging apps, communication has become more efficient. Seniors can stay in touch with loved
+                ones, and
+                caregivers now have new ways to check in on patients or aging parents.
+
+                Hospital and clinics appointment is an online website, accessible through the browser and mobile
+                devices, to
+                make appointment easier and hassle- free. This website requires the user to turn on their location to
+                find the
+                best recommended/clinics through various multiple statistics in regards with the user on that specific
+                location.
+                Once appointment is booked, the doctor can only interact with or notify the patient to the patient
+            </p>
+
+            <h2>Project Context</h2>
+
+            <p style="white-space: pre-wrap;">Patient’s appointment with doctors for schedule is one of the activities
+                that are used in the hospital and
+                clinics,
+                In this project, the researchers would like to make a system that can help patient appoint their
+                schedules
+                anytime or anywhere. A system that would be useful to patients, especially senior and PWD, this presents
+                features where patients can select their doctor with a specific specialization, set check – up
+                appointment, and
+                digital prescription, and patients can message or interact with their doctors. This web-based system
+                provides
+                the best friendly user and easy-to-use system hence can be useful to such users who are seniors and PWDs
+                that
+                are having hard time in going to clinics and hospitals</p>
+
+
+            <h2>Purpose and Description</h2>
+
+            <p style="white-space: pre-wrap;">The sole purpose of this study is to build a web-based system for patients
+                to select their doctor with a
+                specific specialization, set check-up appointments, and digital prescription, and patients can message
+                or
+                interact with their doctors. Being able to apply our systems to target locations which are hospitals and
+                clinics
+                to be able to make a good condition where we can help the target location with ways to make their
+                patient
+                hassle-free, convenient, and more industrialized so that they can be able to seek medical help at just
+                their
+                fingertips. Where the same quality health care services are provided as they conveniently book
+                appointments,
+                make online consulations, and receive digital prescriptions.
+                In order for researchers to see how our system could be enchanced during the test run, the approach that
+                we are
+                going to apply to its features will be put to the tests, so that individuals can benefits from the
+                technological
+                advancement that will be made during the deployment of our system on the target location.
+            </p>
+
+            <h2>Objectives</h2>
+            <ul>
+                <li>This web application helps easily get an appointmet to specific hospital without a hassle.</li>
+                <li>To reduce physical waiting time in a specific hopistal.</li>
+                <li>The developers aimed to this study to create a friendly user web application that handles data
+                    effectively and
+                    accurately, as well as giving a doctor to select a specialization and easily gives consultant.</li>
+            </ul>
+
+            <h2>Scope and Delimitation</h2>
+
+            <p style="white-space: pre-wrap;">The focus of this capstone study is to help patients set their appointment
+                which are accessible on our site,
+                mainly to help the patients with PWD and also patients who are far from civilization. This website will
+                be
+                available in a specific area within the country. Doctor and hospital availability across the country it
+                depends
+                on the hospital or clinics doctor avail <strong>Medic Search</strong> services.</p>
+        </div>
+    </section>
+    <FooterBlock />
+</template>
+<style scoped>
+.section {
+    background: center no-repeat url('../../assets/images/background-client-profile.png');
+    background-size: cover;
+}
+</style>
