33f6653cda9914ff3c7af151375c7449ebe8351c
Leindfraust <rozer223@gmail.com>
2022-10-26 21:06:44 +0800

    feat: added component for catching errors

---
+++ client/src/components/CatchError.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    errMsg: null
+});
+
+const errMsg = computed(() => props.errMsg)
+</script>
+<template>
+    <div class="notification is-danger" v-if="errMsg">Sorry, something went wrong. {{ errMsg.message }}, please
+        try again later or
+        <router-link :to="'/contactus'">contact us</router-link>.
+    </div>
+</template>
