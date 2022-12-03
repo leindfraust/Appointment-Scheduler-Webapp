2c49412b7a6180a3281c470eb255b946be09be02
Leindfraust <rozer223@gmail.com>
2022-12-03 17:12:16 +0800

    chore: dist build update
    General changes
    
    - improved UI
    - bugfixes
    - added testing
    - performance improvements
    
    New features
    
    - added payment methods in appointments

---
+++ client/dist/js/970.18629a47.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[970,295],{2750:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var u=t(6252),r=t(2262),c=t(1458),s={__name:"UserView",setup(e){return(e,n)=>((0,u.wg)(),(0,u.j4)((0,r.SU)(c["default"])))}};const a=s;var f=a}}]);
+//# sourceMappingURL=970.18629a47.js.map
