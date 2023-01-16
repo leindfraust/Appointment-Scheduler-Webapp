2a3f9b0ef4c001862dad1797e0e00e638fcc4f62
Leindfraust <rozer223@gmail.com>
2023-01-16 22:27:02 +0800

    refactor: converted options API to composition API

--- client/src/components/NavigationTab.vue
+++ client/src/components/NavigationTab.vue
@@ -1,3 +1,87 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import socket from '../socket'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const props = defineProps({
+    providerFinderMode: {
+        type: Boolean,
+        default: false
+    },
+    providerProvinces: {
+        type: Array,
+        default: undefined
+    },
+    providerList: {
+        type: undefined,
+        default: undefined
+    },
+    query: String
+})
+defineEmits(['unloadPropData'])
+onMounted(async () => {
+    await axios.get(`https://res.cloudinary.com/leindfraust/image/upload/v1/assets/patients/${store.state.patientUsername}.jpg`).then(response => response.status == 200 ? store.commit('checkProfileImg', true) : store.commit('checkProfileImg', false)).catch(err => err ? store.commit('checkProfileImg', false) : store.commit('checkProfileImg', true))
+    checkImgState.value = true
+
+    if (store.state.patientID) {
+        socket.connect()
+        socket.emit('join room', store.state.patientID)
+        socket.on('send messages', (response) => {
+            notifications.value = response
+        });
+        socket.on('delete messages', (response) => {
+            notifications.value = response
+        });
+        socket.on('messages', (response) => {
+            notifications.value = response
+        });
+    }
+})
+
+const isActiveNotifications = ref(false)
+const isActiveModal = ref(false)
+const isActiveProfileDropdown = ref(false)
+//const isActiveMenuDropdown = ref(false)
+const isActive = ref(false)
+const patient = store.state.patientUsername
+const notifications = ref([])
+const viewNotif = ref([])
+const checkImgState = ref(false)
+const queryInput = ref(props.query)
+
+function navbar() {
+    isActive.value = !isActive.value
+}
+async function logout() {
+    store.commit("patientID", null);
+    store.commit("patientUsername", '');
+    await axios.delete("/session/patient");
+    await this.$router.push("");
+    socket.disconnect();
+}
+function notification() {
+    isActiveNotifications.value = !isActiveNotifications.value
+}
+async function deleteNotif(notif) {
+    await axios.post('/api/imgUploadImgMsgDeletePatient', {
+        id: 'assets/patientimgmsg/patientCopy/' + notif.id
+    });
+    socket.emit('delete message', store.state.patientID, notif)
+}
+function openNotif(notif, index) {
+    viewNotif.value = notif
+    isActiveModal.value = true
+    if (viewNotif.value.new) {
+        notifications.value[index].new = false
+        socket.emit('update message', notifications.value)
+    }
+}
+function closeNotificationModal() {
+    isActiveModal.value = false
+}
+</script>
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation"
         :class="{ 'provider-finder-mode': providerFinderMode }"
@@ -6,6 +90,8 @@
             <a class="navbar-item" href="/">
                 <img src="../assets/images/logos/medi-search-horizontal.png" />
             </a>
+            <router-link :to="'/about'" class="navbar-item is-hidden-desktop is-hidden-tablet">About</router-link>
+            <router-link :to="'/contactus'" class="navbar-item is-hidden-desktop is-hidden-tablet">Contact</router-link>
             <a role="button" class="navbar-burger" :class="{ 'is-active': isActive }" aria-label="menu"
                 aria-expanded="false" data-target="navbar" @click="navbar">
                 <span aria-hidden="true"></span>
@@ -13,54 +99,49 @@
                 <span aria-hidden="true"></span>
             </a>
         </div>
-        <div class="navbar-menu" v-if="providerFinderMode">
-            <div class="field has-addons navbar-item" style="margin-left: auto">
+        <div class="navbar-menu">
+            <div class="navbar-start">
+                <router-link :to="'/about'" class="navbar-item">About</router-link>
+                <router-link :to="'/contactus'" class="navbar-item">Contact</router-link>
+            </div>
+            <div class="field has-addons navbar-item" style="margin-left: auto" v-if="providerFinderMode">
                 <div class="control has-icons-left">
-                    <input class="input is-rounded" type="text" v-model="provinceInput" style="width: 350px;"
-                        placeholder="What province are you located?" list="provinces" />
+                    <input class="input is-rounded" type="text" v-model="queryInput" style="width: 350px;"
+                        placeholder="Search..." list="providers" @keyup.enter="$emit('unloadPropData', queryInput)" />
                     <span class="icon is-small is-left has-text-info">
                         <i class="fa-solid fa-location-dot"></i>
                     </span>
                 </div>
-                <datalist id="provinces">
+                <datalist id="providers">
                     <option v-for="geodata in providerProvinces" :key="geodata._id">
                         {{ geodata.province }}</option>
+                    <option v-for="provider in providerList" :key="provider._id">
+                        {{ provider.hospital }}</option>
                 </datalist>
                 <div class="control">
-                    <button class="button is-info is-rounded" :disabled="provinceInput == ''"
-                        @click="$emit('loadProvider', provinceInput)">Search</button>
+                    <button class="button is-info is-rounded" :disabled="queryInput == ''"
+                        @click="$emit('unloadPropData', queryInput)">Search</button>
                 </div>
             </div>
         </div>
         <div id="navbar" class="navbar-menu" :class="{ 'is-active': isActive }">
-            <div class="navbar-start is-hidden-desktop">
-                <div v-if="providerFinderMode">
-                    <div class="field has-addons navbar-item" style="margin-left: auto">
-                        <div class="control has-icons-left">
-                            <input class="input is-rounded" type="text" v-model="provinceInput"
-                                placeholder="What province are you located?" list="provinces" />
-                            <span class="icon is-small is-left has-text-info">
-                                <i class="fa-solid fa-location-dot"></i>
-                            </span>
-                        </div>
-                        <datalist id="provinces">
-                            <option v-for="geodata in providerProvinces" :key="geodata._id">
-                                {{ geodata.province }}</option>
-                        </datalist>
-                        <div class="control">
-                            <button class="button is-info is-rounded" :disabled="provinceInput == ''"
-                                @click="$emit('loadProvider', provinceInput)">Search</button>
-                        </div>
-                    </div>
+            <div class="field has-addons navbar-item is-hidden-desktop is-hidden-tablet" v-if="providerFinderMode">
+                <div class="control has-icons-left">
+                    <input class="input is-rounded" type="text" v-model="queryInput" style="width: 200px;"
+                        placeholder="Search..." list="providers" @keyup.enter="$emit('unloadPropData', queryInput)" />
+                    <span class="icon is-small is-left has-text-info">
+                        <i class="fa-solid fa-location-dot"></i>
+                    </span>
                 </div>
-                <div class="navbar-item">
-                    <router-link :to="'/doctor/login'" class="navbar-item">Doctor Login</router-link>
-                    <router-link :to="'/manager/login'" class="navbar-item">Provider Login</router-link>
-                </div>
-                <hr>
-                <div class="navbar-item">
-                    <router-link :to="'/about'" class="navbar-item">About</router-link>
-                    <router-link :to="'/contactus'" class="navbar-item">Contact</router-link>
+                <datalist id="providers">
+                    <option v-for="geodata in providerProvinces" :key="geodata._id">
+                        {{ geodata.province }}</option>
+                    <option v-for="provider in providerList" :key="provider._id">
+                        {{ provider.hospital }}</option>
+                </datalist>
+                <div class="control">
+                    <button class="button is-info is-rounded" :disabled="queryInput == ''"
+                        @click="$emit('unloadPropData', queryInput)">Search</button>
                 </div>
             </div>
             <div class="navbar-end" v-if="patient != ''">
@@ -68,7 +149,8 @@
                     <a class="navbar-link" @click="notification"><span class="has-text-danger"
                             v-if="notifications.find(x => x.new === true)"><i
                                 class="fa-solid fa-bell animate__animated animate__heartBeat animate__infinite"><span
-                                    class="is-size-7">{{ notifications.filter(x =>
+                                    class="is-size-7">{{
+                                        notifications.filter(x =>
                                             x.new).length
                                     }}</span></i></span><span class="has-text-info" v-else><i
                                 class="fa-solid fa-bell"></i></span></a>
@@ -78,7 +160,8 @@
                         })" :key="index">
                             <div class="notification is-info" :class="{ 'is-light': !notifs.new }" style="width: 20rem">
                                 <a style="text-decoration: none;" @click="openNotif(notifs, index)">{{ notifs.subject }}
-                                    <p class="help">{{ notifs.from == 'Medic Search' ? `From ${notifs.from}` : `From
+                                    <p class="help">{{
+                                        notifs.from == 'Medic Search' ? `From ${notifs.from}` : `From
                                                                             Dr. ${notifs.from}`
                                     }}</p>
                                 </a>
@@ -93,13 +176,13 @@
                 <div class="navbar-item has-dropdown" :class="{ 'is-active': isActiveProfileDropdown }"
                     @click="isActiveProfileDropdown = !isActiveProfileDropdown">
                     <a class="navbar-item is-hidden-mobile" v-if="checkImgState">
-                        <figure class="image" v-if="$store.state.checkProfileImg">
+                        <figure class="image" v-if="store.state.checkProfileImg">
                             <img class="is-rounded"
                                 :src="`https://res.cloudinary.com/leindfraust/image/upload/v${new Date().getMonth()}${new Date().getDate()}/assets/patients/${$store.state.patientUsername}.jpg`">
                         </figure>
                         <figure class="image" v-else>
                             <img class="is-rounded"
-                                :src="`https://ui-avatars.com/api/?name=${this.$store.state.patientUsername}`">
+                                :src="`https://ui-avatars.com/api/?name=${store.state.patientUsername}`">
                         </figure>
                     </a>
                     <div class="navbar-dropdown is-right">
@@ -111,38 +194,16 @@
                         </a>
                     </div>
                 </div>
-                <div class="navbar-item has-dropdown is-hidden-mobile" :class="{ 'is-active': isActiveMenuDropdown }">
-                    <a class="navbar-item" @click="isActiveMenuDropdown = !isActiveMenuDropdown"><i
-                            class="fa-solid fa-bars"></i></a>
-                    <div class="navbar-dropdown is-right">
-                        <router-link :to="'/doctor/login'" class="navbar-item">Doctor Login</router-link>
-                        <router-link :to="'/manager/login'" class="navbar-item">Provider Login</router-link>
-                        <hr>
-                        <router-link :to="'/about'" class="navbar-item">About</router-link>
-                        <router-link :to="'/contactus'" class="navbar-item">Contact Us</router-link>
-                    </div>
-                </div>
             </div>
             <div class="navbar-end" v-else>
                 <div class="navbar-item">
-                    <router-link :to="'/user/login'" class="button is-dark is-inverted"
-                        style="background-color: transparent">Log in</router-link>
+                    <router-link :to="'/account/login'" class="button is-dark is-inverted is-medium"
+                        style="background-color: transparent">Sign In</router-link>
                 </div>
                 <div class="navbar-item">
-                    <router-link :to="'/user/signup'" class="button is-info">
+                    <router-link :to="'/user/signup'" class="button is-info is-medium">
                         <strong>Sign up</strong>
                     </router-link>
-                </div>
-                <div class="navbar-item has-dropdown is-hidden-mobile" :class="{ 'is-active': isActiveMenuDropdown }">
-                    <a class="navbar-item" @click="isActiveMenuDropdown = !isActiveMenuDropdown"><i
-                            class="fa-solid fa-bars"></i></a>
-                    <div class="navbar-dropdown is-right">
-                        <router-link :to="'/doctor/login'" class="navbar-item">Doctor Login</router-link>
-                        <router-link :to="'/manager/login'" class="navbar-item">Provider Login</router-link>
-                        <hr>
-                        <router-link :to="'/about'" class="navbar-item">About</router-link>
-                        <router-link :to="'/contactus'" class="navbar-item">Contact Us</router-link>
-                    </div>
                 </div>
             </div>
         </div>
@@ -152,7 +213,8 @@
         <div class="modal-background"></div>
         <div class="modal-content box">
             <section class="section">
-                <p class="title">{{ viewNotif.from == 'Medic Search' ? `From ${viewNotif.from}` : `From
+                <p class="title">{{
+                    viewNotif.from == 'Medic Search' ? `From ${viewNotif.from}` : `From
                                     Dr. ${viewNotif.from}`
                 }}</p>
                 <p class="subtitle">{{ new Date(viewNotif.date).toDateString() }}</p>
@@ -172,89 +234,6 @@
         <button class="modal-close is-large" aria-label="close" @click="closeNotificationModal"></button>
     </div>
 </template>
-<script>
-import axios from 'axios'
-import socket from '../socket'
-export default {
-    name: 'NavigationTab',
-    emits: ['loadProvider'],
-    props: {
-        providerFinderMode: {
-            type: Boolean,
-            default: false
-        },
-        providerProvinces: {
-            type: Array,
-            default: undefined
-        },
-        provinceSelected: String
-    },
-    async mounted() {
-        await axios.get(`https://res.cloudinary.com/leindfraust/image/upload/v1/assets/patients/${this.$store.state.patientUsername}.jpg`).then(response => response.status == 200 ? this.$store.commit('checkProfileImg', true) : this.$store.commit('checkProfileImg', false)).catch(err => err ? this.$store.commit('checkProfileImg', false) : this.$store.commit('checkProfileImg', true))
-        this.checkImgState = true
-
-        if (this.$store.state.patientID) {
-            socket.connect()
-            socket.emit('join room', this.$store.state.patientID)
-            socket.on('send messages', (response) => {
-                this.notifications = response
-            });
-            socket.on('delete messages', (response) => {
-                this.notifications = response
-            });
-            socket.on('messages', (response) => {
-                this.notifications = response
-            });
-        }
-    },
-    data() {
-        return {
-            isActiveNotifications: false,
-            isActiveModal: false,
-            isActiveProfileDropdown: false,
-            isActiveMenuDropdown: false,
-            isActive: false,
-            patient: this.$store.state.patientUsername,
-            notifications: [],
-            viewNotif: [],
-            checkImgState: false,
-            provinceInput: this.provinceSelected
-        }
-    },
-    methods: {
-        navbar() {
-            this.isActive = !this.isActive
-        },
-        async logout() {
-            await this.$store.commit("patientID", null);
-            await this.$store.commit("patientUsername", '');
-            await axios.delete("/session/patient");
-            await this.$router.push("/user/login");
-            socket.disconnect();
-        },
-        notification() {
-            this.isActiveNotifications = !this.isActiveNotifications
-        },
-        async deleteNotif(notif) {
-            await axios.post('/api/imgUploadImgMsgDeletePatient', {
-                id: 'assets/patientimgmsg/patientCopy/' + notif.id
-            });
-            socket.emit('delete message', this.$store.state.patientID, notif)
-        },
-        openNotif(notif, index) {
-            this.viewNotif = notif
-            this.isActiveModal = true
-            if (this.viewNotif.new) {
-                this.notifications[index].new = false
-                socket.emit('update message', this.notifications)
-            }
-        },
-        closeNotificationModal() {
-            this.isActiveModal = false
-        }
-    }
-}
-</script>
 <style scoped>
 .notif-list {
     max-height: 20em;
