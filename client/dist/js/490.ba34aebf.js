432e7a9abf53933c2a8bc13be2a313db52174761
Leindfraust <rozer223@gmail.com>
2023-03-03 11:06:32 +0800

    chore: dist build update
    Server version: 1.2.1
    
    Changes
    - updated packages for openAI and use modern openAI fetching API(experimental)
    
    Client version: 1.3.2
    
    Changes
    - added loading button for updating doctor information
    - added EULA page
    - fixed fullname schema in session not updating
    - added callback error for openAI fetching of recommendation

--- client/dist/js/490.ba34aebf.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[490,810],{2750:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var u=t(6252),r=t(2262),c=t(4420),s={__name:"UserView",setup(e){return(e,n)=>((0,u.wg)(),(0,u.j4)((0,r.SU)(c["default"])))}};const a=s;var f=a}}]);
-//# sourceMappingURL=490.ba34aebf.js.map
