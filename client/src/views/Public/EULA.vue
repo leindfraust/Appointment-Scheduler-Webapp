9c77e82474e6ba7c98d22a891f1a7e851d798d58
Leindfraust <rozer223@gmail.com>
2023-03-03 11:00:41 +0800

    feat: added EULA page

---
+++ client/src/views/Public/EULA.vue
@@ -0,0 +1,67 @@
+<script setup>
+import NavigationTab from '../../components/NavigationTab.vue';
+import FooterBlock from '../../components/FooterBlock.vue';
+</script>
+<template>
+    <NavigationTab />
+    <section class="section">
+        <div class="content">
+            <h1>END USER LICENSE AGREEMENT</h1>
+
+            <p style="white-space: pre-wrap;">
+                This End User License Agreement (the "Agreement") is a legal agreement between you (either an individual
+                or a single entity) and <b>Medic Search</b>, the operator of the website located at www.medicsearch.com (the
+                "Website"). By accessing or using the Website, you are indicating that you have read, understand, and
+                agree to be bound by the terms and conditions of this Agreement. If you do not agree to the terms and
+                conditions of this Agreement, you may not use the Website.
+            </p>
+
+            <ul>
+                <li><b>Grant of License</b>. <b>Medic Search</b> grants you a non-transferable, non-exclusive, revocable
+                    license to access and use the Website for your own personal, non-commercial use. This license is
+                    subject to the terms and conditions of this Agreement and will terminate automatically if you
+                    violate any of the terms and conditions of this Agreement.</li>
+                <li><b>Restrictions</b>. You agree not to: (a) use the Website for any commercial purpose; (b) use the
+                    Website for any purpose that is illegal or prohibited by this Agreement; (c) modify, adapt,
+                    translate, or reverse engineer any portion of the Website; (d) remove any proprietary notices or
+                    labels on the Website; or (e) use any robot, spider, or other automated device to access the
+                    Website.</li>
+                <li><b>Disclaimer of Warranties</b>. The Website is provided on an "as is" and "as available" basis.
+                    <b>Medic Search</b> makes no representations or warranties of any kind, express or implied, as to the
+                    operation of the Website or the information, content, materials, or products included on the
+                    Website. <b>Medic Search</b> does not warrant that the Website will be uninterrupted or error-free, and
+                    <b>Medic Search</b> will not be liable for any interruptions or errors.</li>
+                <li><b>Limitation of Liability</b>. In no event shall <b>Medic Search</b> be liable for any damages whatsoever,
+                    including but not limited to any direct, indirect, special, incidental, or consequential damages,
+                    arising out of or in connection with the use or inability to use the Website.</li>
+                <li><b>Indemnification</b>. You agree to indemnify and hold <b>Medic Search</b>, its subsidiaries, affiliates,
+                    officers, agents, and employees harmless from any claim or demand, including reasonable attorneys'
+                    fees, made by any third party due to or arising out of your use of the Website, your violation of
+                    this Agreement, or your violation of any rights of another.</li>
+                <li><b>Modification of this Agreement</b>. <b>Medic Search</b> reserves the right, at its sole discretion, to
+                    modify or replace any part of this Agreement. It is your responsibility to check this Agreement
+                    periodically for changes. Your continued use of or access to the Website following the posting of
+                    any changes to this Agreement constitutes acceptance of those changes.
+                </li>
+                <li><b>Termination of License</b>. <b>Medic Search</b> may terminate this license at any time for any reason,
+                    without notice. In the event of termination, you will no longer be authorized to access or use the
+                    Website.
+                </li>
+                <li><b>Governing Law</b>. This Agreement shall be governed by and construed in accordance with the laws
+                    of the country of the <b>Medic Search</b> without giving effect to any principles of conflicts of law.</li>
+                <li><b>Entire Agreement</b>. This Agreement constitutes the entire agreement between you and Medic
+                    Search with respect to the use of the Website and supersedes all prior or contemporaneous
+                    communications and proposals, whether oral or written, between you and <b>Medic Search</b>.</li>
+                <li><b>Contact</b>. If you have any questions about this Agreement or the Website, please contact Medic
+                    Search at romaygbriecloy@hotmail.com</li>
+            </ul>
+        </div>
+        <FooterBlock />
+    </section>
+</template>
+<style scoped>
+.section {
+    background: center no-repeat url('../../assets/images/background-client-profile.png');
+    background-size: cover;
+}
+</style>
