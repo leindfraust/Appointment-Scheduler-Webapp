3f2eb0615fbffb6edce1274f8e4394cfe211250d
Leindfraust <rozer223@gmail.com>
2023-04-24 08:27:00 +0800

    fix: converted mongoose operation to try catch block

--- controllers/superuserController.js
+++ controllers/superuserController.js
@@ -60,31 +60,27 @@ const submitTicket = (async (req, res) => {
     let email = req.body.email
     let ticketID = req.body.ticketID
     let ticketMessage = req.body.ticketMessage
-    Superuser.findOneAndUpdate({
-        _id: '6187884b847ca9434716e7ba'
-    }, {
-        $push: {
-            tickets: {
-                id: ticketID,
-                type: ticketType,
-                email: email,
-                title: ticketTitle,
-                message: ticketMessage,
-                date: new Date(),
-                active: true
-            }
+    try {
+        const superuser = await Superuser.findOneAndUpdate({
+            _id: '6187884b847ca9434716e7ba'
+        }, {
+            $push: {
+                tickets: {
+                    id: ticketID,
+                    type: ticketType,
+                    email: email,
+                    title: ticketTitle,
+                    message: ticketMessage,
+                    date: new Date(),
+                    active: true
+                }
 
-        }
-    }, {
-        returnOriginal: false
-    }, function (error, success) {
-        if (error) {
-            console.log(error)
-        } else {
-            console.log(success)
-            res.end()
-        }
-    });
+            }
+        }, { returnOriginal: false })
+        res.status(200).send(superuser)
+    } catch (err) {
+        res.status(500).send(err)
+    }
 });
 
 const deleteTicket = (async (req, res) => {
