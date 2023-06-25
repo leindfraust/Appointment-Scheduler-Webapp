3188f7c1f6a04af25797319fd68ebcbe1d84bf4d
Leindfraust <rozer223@gmail.com>
2023-06-25 19:43:07 +0800

    refactor: added global css for doctor components

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,4 +16,17 @@ input[type=number]::-webkit-outer-spin-button {
   -webkit-appearance: none;
   margin: 0;
 }
+/* global css for doctor */
+.main-doctor::-webkit-scrollbar {
+  display: none;
+}
+
+.main-doctor-content {
+  max-height: 100vh;
+  overflow: auto;
+}
+
+.main-doctor {
+  background: linear-gradient(180deg, #DAE6FE 0%, #FFFFFF 43.56%);
+}
 </style>
