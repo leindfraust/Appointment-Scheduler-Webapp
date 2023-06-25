be8c8d3287f5b938bc965fe964550638907700ad
Leindfraust <rozer223@gmail.com>
2023-06-25 19:48:36 +0800

    refactor: changed Home component to HomeView

---
+++ client/src/views/Public/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import ProviderFinder from "../../components/ProviderFinder.vue";
+import FooterBlock from "../../components/FooterBlock.vue";
+</script>
+<template>
+  <ProviderFinder />
+  <FooterBlock />
+</template>
