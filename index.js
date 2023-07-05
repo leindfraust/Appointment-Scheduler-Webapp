873c00a45b0ec9c6e133c86f229260300574c18e
Leindfraust <rozer223@gmail.com>
2023-07-06 00:35:19 +0800

    Revert "fix: cannot upload pic on render"
    This reverts commit fec8de17e9d3b36ee629758ae91f898beadd4119.

--- index.js
+++ index.js
@@ -49,11 +49,10 @@ app.use(helmet({
     contentSecurityPolicy: {
         useDefault: true,
         directives: {
-            "img-src": ["'self'", "data:", "https://res.cloudinary.com", "https://ui-avatars.com"],
+            "img-src": ["'self'", "data:", "https://res.cloudinary.com", "https://ui-avatars.com", "https://medic-search.onrender.com/", "https://render.com/", "*.https://medic-search.onrender.com/"],
             "connect-src": ["'self'", "https://use.fontawesome.com", "https://ka-f.fontawesome.com", "https://res.cloudinary.com", "https://api.paymongo.com"],
             "script-src": ["'self'", "https://use.fontawesome.com", "maps.googleapis.com", "https://kit.fontawesome.com/4ca5ad86da.js"],
-            "frame-src": ["'self'", "https://www.google.com/maps", "https://maps.google.com/"],
-            "object-src": ["'self'", "blob:"]
+            "frame-src": ["'self'", "https://www.google.com/maps", "https://maps.google.com/"]
         }
     },
     crossOriginResourcePolicy: {
