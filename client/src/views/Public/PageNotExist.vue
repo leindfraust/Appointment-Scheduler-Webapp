f43ccfcecdecd7bad883f6581c2d5b9c8d8c737d
Leindfraust <rozer223@gmail.com>
2023-07-05 11:58:59 +0800

    refactor: reload when an account is authenticated before

--- client/src/views/Public/PageNotExist.vue
+++ client/src/views/Public/PageNotExist.vue
@@ -1,10 +1,26 @@
 <script setup>
+import { ref, onBeforeMount } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
 
+const router = useRouter()
+const store = useStore()
+const authCheck = ref(true)
+//reload once if authenticating of an account results in error
+onBeforeMount(async () => {
+    console.log(store.state.managerHospital)
+    if (await store.state.patientUsername || await store.state.alias || await store.state.managerHospital) {
+        router.go()
+    } else {
+        authCheck.value = false
+
+    }
+})
 </script>
 <template>
-<section class="section">
-    <div class="container">
-        <h1 class="title">Oops, page does not exist. Please return here <router-link :to="'/'">here.</router-link></h1>
-    </div>
-</section>
+    <section class="section" v-if="!authCheck">
+        <div class="container">
+            <h1 class="title">Oops, page does not exist. Please return <router-link :to="'/'">here.</router-link></h1>
+        </div>
+    </section>
 </template>
